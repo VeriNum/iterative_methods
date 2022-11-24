2a0c2e2e63c3179d2ae6f05068bafc8bd817d173
Mohit Tekriwal <tmohit@umich.edu>
2022-11-24 16:01:46 +0100

    Proved theorem on supremum of bound

--- iter_theorem.v
+++ iter_theorem.v
@@ -60,10 +60,114 @@ Definition tau {n:nat} (k:nat) (x: 'cV[R]_n.+1)
   let E_2 := mat_mat_mult_err_bnd inv_A1 A2 in
   let S_hat := (-f (inv_A1 *f A2)) in
   let E_3 := mat_vec_mult_err_bnd S_hat (x_hat k) in
-  (matrix_inf_norm S * d + E_2 * d + E_2) * (theta_x k x_hat x) * vec_inf_norm x + 
-  (E_3 * d + (vec_inf_norm f) * d + E_1* d + E_3 + E_1 + e).
+  ((matrix_inf_norm S * d + E_2 * d + E_2) * ((theta_x k x_hat x) * vec_inf_norm x) + 
+  (E_3 * d + (vec_inf_norm f) * d + E_1* d + E_3 + E_1 + e))%Re.
 
 
+
+Definition tau_m {n:nat} (k:nat) (x: 'cV[R]_n.+1) 
+  (x_hat : nat ->'cV[ftype Tsingle]_n.+1)
+  (inv_A1 A1 A2 : 'M[ftype Tsingle]_n.+1)
+  (b : 'cV[ftype Tsingle]_n.+1):=
+  let e := / 2 * Raux.bpow Zaux.radix2 (3 - femax Tsingle - fprec Tsingle) in
+  let d := / 2 * Raux.bpow Zaux.radix2 (- fprec Tsingle + 1) in
+  let S := - (FT2R_mat inv_A1) * (FT2R_mat A2) in 
+  let f := FT2R_mat (inv_A1) *m (FT2R_mat b) in
+  let E_1 := mat_vec_mult_err_bnd inv_A1 b in 
+  let E_2 := mat_mat_mult_err_bnd inv_A1 A2 in
+  let S_hat := (-f (inv_A1 *f A2)) in
+  let E_3 := mat_vec_mult_err_bnd S_hat (x_hat k) in
+  ((matrix_inf_norm S * d + E_2 * d + E_2) * vec_inf_norm (FT2R_mat (x_hat k)) + 
+  (E_3 * d + (vec_inf_norm f) * d + E_1* d + E_3 + E_1 + e))%Re.
+
+
+Lemma pow_invert_2: forall x y z :R,
+  (0 < z)%Re ->  (x / z <= y)%Re -> (x <= y * z)%Re.
+Proof.
+intros.
+replace x with (x * 1)%Re by nra.
+assert (1%Re = (/z * z)%Re).
+{ symmetry. apply Rinv_l. nra. } rewrite H1.
+replace (x * (/z * z))%Re with ((x / z) * z)%Re by nra.
+apply Rmult_le_compat_r.
++ nra.
++ nra.
+Qed.
+
+
+Lemma vec_norm_definite {n:nat} (v : 'cV[R]_n.+1):
+  v != 0 ->
+  (0 < vec_inf_norm v)%Re.
+Proof.
+intros.
+unfold vec_inf_norm.
+assert (exists i, v i 0 != 0).
+{ by apply /cV0Pn. } destruct H0 as [i H0].
+apply Rlt_le_trans with 
+[seq Rabs (v i0 0) | i0 <- enum 'I_n.+1]`_i.
++ rewrite seq_equiv. rewrite nth_mkseq; last by []. 
+  apply Rabs_pos_lt. apply /eqP. 
+  by rewrite inord_val.
++ apply /RleP. apply (@bigmaxr_ler _ 0%Re [seq Rabs (v i0 0) | i0 <- enum 'I_n.+1] i).
+  by rewrite size_map size_enum_ord.
+Qed.
+
+Lemma mat_err_bnd_pd {n:nat}
+  (A1 A2 : 'M[ftype Tsingle]_n.+1) :
+  0%Re <= mat_mat_mult_err_bnd A1 A2.
+Proof.
+unfold mat_mat_mult_err_bnd.
+apply bigmax_le_0.
++ apply /RleP; apply Rle_refl.
++ intros. 
+  rewrite seq_equiv. 
+  rewrite nth_mkseq; last by rewrite size_map size_enum_ord in H.
+  unfold row_e_j.
+  apply big_ge_0_ex_abstract.
+  intros. apply /RleP. unfold e_i.
+  apply Rplus_le_le_0_compat.
+  - apply Rplus_le_le_0_compat.
+    * apply Rmult_le_pos.  unfold dot_prodR.
+      apply sum_abs_pair_rel. apply Rge_le. apply Rge_minus. apply Rle_ge.
+      apply pow_R1_Rle. rewrite -!RmultE. simpl; nra.
+    * repeat apply Rmult_le_pos.
+      ++ apply pos_INR.
+      ++ nra.
+      ++ simpl; nra.
+      ++ apply pow_le . rewrite -!RmultE. simpl;nra.
+  - rewrite -!RmultE. apply Rmult_le_pos.
+    * apply Rmult_le_pos.
+      ++ simpl;nra.
+      ++ apply Rge_le. apply Rge_minus. apply Rle_ge.
+         apply pow_R1_Rle. simpl; nra.
+    * simpl;nra.
+Qed.
+
+
+Lemma tau_bounds_tau_m {n:nat} (k:nat) (x: 'cV[R]_n.+1) 
+  (x_hat : nat ->'cV[ftype Tsingle]_n.+1)
+  (inv_A1 A1 A2 : 'M[ftype Tsingle]_n.+1)
+  (b : 'cV[ftype Tsingle]_n.+1) :
+  x != 0 ->
+  (tau_m  k x x_hat inv_A1 A1 A2 b <= tau k x x_hat inv_A1 A1 A2 b)%Re.
+Proof.
+intros.
+unfold tau_m, tau.
+apply Rplus_le_compat_r. 
+apply Rmult_le_compat_l.
++ repeat apply Rplus_le_le_0_compat. 
+  - apply Rmult_le_pos.
+    * apply /RleP. apply matrix_norm_pd.
+    * rewrite -RmultE. simpl;nra.
+  - apply Rmult_le_pos.
+    * apply /RleP. apply mat_err_bnd_pd.
+    * rewrite -RmultE. simpl;nra.
+  - apply /RleP. apply mat_err_bnd_pd.
++ unfold theta_x. apply pow_invert_2. 
+  - by apply vec_norm_definite.
+  - remember (vec_inf_norm (FT2R_mat (x_hat k)) / vec_inf_norm x)%Re as v.
+    rewrite sup1. nra.
+Qed.
 
 (** not entirely in correct form. need to connect A, A1^{-1}. A2 **)
 Theorem iterative_round_off_error {n:nat} :
