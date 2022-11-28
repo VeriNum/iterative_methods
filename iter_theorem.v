41e841e7d4a850bceff99d024612c8cc66789994
Mohit Tekriwal <tmohit@umich.edu>
2022-11-28 11:40:51 +0100

    refined the error bound E_3

--- iter_theorem.v
+++ iter_theorem.v
@@ -48,6 +48,72 @@ Definition theta_x  {n:nat} (k:nat)
 
 
 
+
+(** Revising the bound E_3 **)
+Fixpoint vec_to_list_real {n:nat} (m:nat) (v :'cV[R]_n.+1)
+   : list R := 
+   match m with 
+   | O => []
+   | S p => [v (@inord n p) ord0] ++ vec_to_list_real p v
+   end.
+
+Definition Rlist_pair_abs (L : list (R * R)) :=
+  map (fun a => (Rabs (fst a), Rabs (snd a))) L.
+
+
+Lemma sum_Rabs_pair_rel:
+  forall (L: list (R * R)),
+  0 <= sum_fixR (prod_fixR (Rlist_pair_abs L)).
+Proof.
+intros.
+induction L.
++ simpl. apply /RleP. apply Rle_refl.
++ simpl. apply /RleP. 
+  apply Rplus_le_le_0_compat.
+  apply Rmult_le_pos; apply Rabs_pos.
+  apply /RleP. apply IHL.
+Qed.
+
+
+Definition e_i_real {n:nat}  (i : 'I_n.+1) (k:nat)
+  (A : 'M[ftype Tsingle]_n.+1) (b : 'cV[ftype Tsingle]_n.+1) 
+  (x_hat : nat -> 'cV[ftype Tsingle]_n.+1)
+  (x : 'cV[R]_n.+1) :=
+  let A_real := FT2R_mat A in
+  let b_real := FT2R_mat b in 
+  let l1 := vec_to_list_real n.+1 (\row_(j < n.+1) A_real i j)^T in
+  let l2 := vec_to_list_real n.+1 x in
+  let L := combine l1 l2 in
+  let rs := (theta_x k x_hat x) * dot_prodR (Rlist_pair_abs L) in
+  let e := / 2 * Raux.bpow Zaux.radix2 (3 - femax Tsingle - fprec Tsingle) in
+  let d := / 2 * Raux.bpow Zaux.radix2 (- fprec Tsingle + 1) in
+  let nr := INR n.+1 in
+  (rs * ((1 + d)^n.+1 -1) + nr * e * (1+d)^(n.+1 -1)%coq_nat + 
+  e * ((1+d)^(n.+1-1)%coq_nat -1) / d)%Re.
+
+
+Definition E_3 {n:nat} (k:nat) 
+  (A : 'M[ftype Tsingle]_n.+1) (b : 'cV[ftype Tsingle]_n.+1) 
+  (x_hat : nat -> 'cV[ftype Tsingle]_n.+1) (x : 'cV[R]_n.+1) :=
+  bigmaxr 0%Re [seq (e_i_real i k A b x_hat x) | i <- enum 'I_n.+1].
+
+
+Definition tau {n:nat} (k:nat) (x: 'cV[R]_n.+1) 
+  (x_hat : nat ->'cV[ftype Tsingle]_n.+1)
+  (inv_A1 A1 A2 : 'M[ftype Tsingle]_n.+1)
+  (b : 'cV[ftype Tsingle]_n.+1):=
+  let e := / 2 * Raux.bpow Zaux.radix2 (3 - femax Tsingle - fprec Tsingle) in
+  let d := / 2 * Raux.bpow Zaux.radix2 (- fprec Tsingle + 1) in
+  let S := - (FT2R_mat inv_A1) * (FT2R_mat A2) in 
+  let f := FT2R_mat (inv_A1) *m (FT2R_mat b) in
+  let E_1 := mat_vec_mult_err_bnd inv_A1 b in 
+  let E_2 := mat_mat_mult_err_bnd inv_A1 A2 in 
+  let S_hat := (-f (inv_A1 *f A2)) in
+  let E_3_def := E_3 k S_hat b x_hat x in 
+  ((matrix_inf_norm S * d + E_2 * d + E_2) * ((theta_x k x_hat x) * vec_inf_norm x) + 
+  (E_3_def * d + (vec_inf_norm f) * d + E_1* d + E_3_def + E_1 + e))%Re.
+
+(*
 Definition tau {n:nat} (k:nat) (x: 'cV[R]_n.+1) 
   (x_hat : nat ->'cV[ftype Tsingle]_n.+1)
   (inv_A1 A1 A2 : 'M[ftype Tsingle]_n.+1)
@@ -62,9 +128,26 @@ Definition tau {n:nat} (k:nat) (x: 'cV[R]_n.+1)
   let E_3 := mat_vec_mult_err_bnd S_hat (x_hat k) in
   ((matrix_inf_norm S * d + E_2 * d + E_2) * ((theta_x k x_hat x) * vec_inf_norm x) + 
   (E_3 * d + (vec_inf_norm f) * d + E_1* d + E_3 + E_1 + e))%Re.
+*)
 
 
+Definition tau_m {n:nat} (k:nat) (x: 'cV[R]_n.+1) 
+  (x_hat : nat ->'cV[ftype Tsingle]_n.+1)
+  (inv_A1 A1 A2 : 'M[ftype Tsingle]_n.+1)
+  (b : 'cV[ftype Tsingle]_n.+1):=
+  let e := / 2 * Raux.bpow Zaux.radix2 (3 - femax Tsingle - fprec Tsingle) in
+  let d := / 2 * Raux.bpow Zaux.radix2 (- fprec Tsingle + 1) in
+  let S := - (FT2R_mat inv_A1) * (FT2R_mat A2) in 
+  let f := FT2R_mat (inv_A1) *m (FT2R_mat b) in
+  let E_1 := mat_vec_mult_err_bnd inv_A1 b in 
+  let E_2 := mat_mat_mult_err_bnd inv_A1 A2 in
+  let S_hat := (-f (inv_A1 *f A2)) in
+  let E_3_def := E_3 k S_hat b x_hat x in
+  ((matrix_inf_norm S * d + E_2 * d + E_2) * vec_inf_norm (FT2R_mat (x_hat k)) + 
+  (E_3_def * d + (vec_inf_norm f) * d + E_1* d + E_3_def + E_1 + e))%Re.
 
+
+(*
 Definition tau_m {n:nat} (k:nat) (x: 'cV[R]_n.+1) 
   (x_hat : nat ->'cV[ftype Tsingle]_n.+1)
   (inv_A1 A1 A2 : 'M[ftype Tsingle]_n.+1)
@@ -79,6 +162,8 @@ Definition tau_m {n:nat} (k:nat) (x: 'cV[R]_n.+1)
   let E_3 := mat_vec_mult_err_bnd S_hat (x_hat k) in
   ((matrix_inf_norm S * d + E_2 * d + E_2) * vec_inf_norm (FT2R_mat (x_hat k)) + 
   (E_3 * d + (vec_inf_norm f) * d + E_1* d + E_3 + E_1 + e))%Re.
+
+*)
 
 
 Lemma pow_invert_2: forall x y z :R,
@@ -230,6 +315,77 @@ intros. rewrite !mxE. by rewrite -addrA.
 Qed.
 
 
+Lemma add_vec_distr_1 {n:nat}:
+  forall a b c: 'cV[R]_n,
+  (a+b) - (b+c) = a - c.
+Proof.
+intros. apply matrixP. unfold eqrel.
+intros. rewrite !mxE. rewrite -!RplusE -!RoppE. nra.
+Qed.
+
+(*
+Lemma tau_rel {n:nat} (k:nat) (x: 'cV[R]_n.+1) 
+  (x_hat : nat ->'cV[ftype Tsingle]_n.+1)
+  (inv_A1 A1 A2 : 'M[ftype Tsingle]_n.+1)
+  (b : 'cV[ftype Tsingle]_n.+1):
+  (tau k x x_hat inv_A1 A1 A2 b <= tau k.+1 x x_hat inv_A1 A1 A2 b)%Re.
+Proof.
+unfold tau. 
+remember (/ 2 *
+   bpow Zaux.radix2 (- fprec Tsingle + 1)) as d.
+remember (/ 2 *
+  bpow Zaux.radix2
+    (3 - femax Tsingle - fprec Tsingle)) as e.
+apply Rplus_le_compat.
++ apply Rmult_le_compat_l.
+  - repeat apply Rplus_le_le_0_compat. 
+    * apply Rmult_le_pos.
+      ++ apply /RleP. apply matrix_norm_pd.
+      ++ rewrite Heqd. rewrite -RmultE. simpl;nra.
+    * apply Rmult_le_pos.
+      ++ apply /RleP. apply mat_err_bnd_pd.
+      ++ rewrite Heqd. rewrite -RmultE. simpl;nra.
+    * apply /RleP. apply mat_err_bnd_pd.
+  - apply Rmult_le_compat_r.
+    ++ apply /RleP. apply vec_norm_pd.
+    ++ admit.
++ repeat apply Rplus_le_compat.
+*)
+
+Lemma E_3_err_bnd_pd {n:nat} (k:nat) 
+  (A : 'M[ftype Tsingle]_n.+1) (b : 'cV[ftype Tsingle]_n.+1) 
+  (x_hat : nat -> 'cV[ftype Tsingle]_n.+1) (x : 'cV[R]_n.+1):
+   x != 0 ->
+  (0 <= E_3 k A b x_hat x)%Re.
+Proof.
+intros.
+unfold E_3. apply /RleP.
+apply bigmax_le_0.
++ apply /RleP. apply Rle_refl.
++ intros. rewrite seq_equiv. rewrite nth_mkseq; last by rewrite size_map size_enum_ord in H0.
+  unfold e_i_real. apply /RleP.
+  apply Rplus_le_le_0_compat.
+  - apply Rplus_le_le_0_compat.
+    * apply Rmult_le_pos.  unfold dot_prodR.
+      apply Rmult_le_pos.
+      ++ by apply theta_x_ge_0.
+      ++ apply /RleP. apply sum_Rabs_pair_rel.
+      ++ apply Rge_le. apply Rge_minus. apply Rle_ge.
+         apply pow_R1_Rle. rewrite -!RmultE. simpl; nra.
+    * repeat apply Rmult_le_pos.
+      ++ apply pos_INR.
+      ++ nra.
+      ++ simpl; nra.
+      ++ apply pow_le . rewrite -!RmultE. simpl;nra.
+  - rewrite -!RmultE. apply Rmult_le_pos.
+    * apply Rmult_le_pos.
+      ++ simpl;nra.
+      ++ apply Rge_le. apply Rge_minus. apply Rle_ge.
+         apply pow_R1_Rle. simpl; nra.
+    * simpl;nra.
+Qed.
+
+
 (** not entirely in correct form. need to connect A, A1^{-1}. A2 **)
 Theorem iterative_round_off_error {n:nat} :
   (1 < n.+1 /\ n.+1< (Z.to_nat (Z.pow_pos 2 23) - 1)%coq_nat)%coq_nat ->
@@ -297,7 +453,7 @@ induction k.
       ++ by apply theta_x_ge_0.
       ++ apply /RleP. apply vec_norm_pd.
   - apply Rmult_le_pos.
-    ++ apply mat_vec_mult_err_bnd_pd .
+    ++ unfold E_3. simpl. by apply E_3_err_bnd_pd .
     ++ unfold d;simpl. rewrite -RmultE. nra.
   - apply Rmult_le_pos.
     ++ apply /RleP. apply vec_norm_pd.
@@ -305,7 +461,7 @@ induction k.
   - apply Rmult_le_pos.
     ++ apply mat_vec_mult_err_bnd_pd .
     ++ unfold d;simpl. rewrite -RmultE. nra.
-  - apply mat_vec_mult_err_bnd_pd .
+  - by apply E_3_err_bnd_pd .
   - apply mat_vec_mult_err_bnd_pd .
   - unfold e;simpl. rewrite -RmultE. nra.
 + (** Induction step **)
@@ -337,10 +493,35 @@ induction k.
     rewrite Rmult_plus_distr_l. 
     apply Rplus_le_compat.
     * admit.
-    * admit.
-
-
-
+    * simpl. 
+      assert (S_mat (FT2R_mat inv_A1) (FT2R_mat A2) *m xkf +
+                FT2R_mat inv_A1 *m b_real -
+                (S_mat (FT2R_mat inv_A1) (FT2R_mat A2) *m xkr +
+                 FT2R_mat inv_A1 *m b_real) = 
+               (S_mat (FT2R_mat inv_A1) (FT2R_mat A2) *m xkf) -
+               (S_mat (FT2R_mat inv_A1) (FT2R_mat A2) *m xkr)).
+      { assert ((S_mat (FT2R_mat inv_A1) (FT2R_mat A2) *m xkr +
+                 FT2R_mat inv_A1 *m b_real) = 
+                FT2R_mat inv_A1 *m b_real + 
+                S_mat (FT2R_mat inv_A1) (FT2R_mat A2) *m xkr).
+        { by rewrite addrC. } rewrite H8. clear H8.
+        by rewrite add_vec_distr_1.
+      } rewrite H8. clear H8.
+      rewrite -mulmxBr.
+      eapply Rle_trans.
+      ++ apply /RleP. apply submult_prop.
+      ++ rewrite -RmultE.
+         match goal with |-context[( _ <= ?a * (?b * ?c))%Re]=>
+          replace (a * (b * c))%Re with (b * (a * c))%Re by nra
+         end. apply Rmult_le_compat_l.
+         -- apply /RleP. apply matrix_norm_pd.
+         -- eapply Rle_trans.
+            ** apply /RleP. apply IHk.
+            ** rewrite -RmultE. apply Rmult_le_compat_r.
+               +++ apply /RleP. apply error_sum_ge_0.
+                   apply matrix_norm_pd.
+               +++ admit.
+        
 Admitted.
 
 
