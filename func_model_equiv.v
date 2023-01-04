43bac824acfcdc050ae52560151353351eedff26
Mohit Tekriwal <tmohit@umich.edu>
2023-01-04 14:32:47 +0100

    updated

--- func_model_equiv.v
+++ func_model_equiv.v
@@ -91,6 +91,67 @@ Definition vector_inj {t} (v: vector t) n  : 'cV[ftype t]_n :=
    \col_(i < n) nth i v (Zconst t 0).
 
 
+(***
+vector_sub b
+  (matrix_vector_mult (remove_diag A) x_n) =
+vec_to_list_float size.+1
+  (\col_j0 (b_v +f
+            -f
+            (A2_J A_v *f
+             vector_inj x_n size.+1)) j0 j)
+***)
+
+Lemma vector_sub_equiv (v1 v2: vector Tsingle) :
+  (0 < length (combine v1 v2))%nat ->
+  let size := (length (combine v1 v2)).-1 in
+  let v1_v := vector_inj v1 size.+1 in 
+  let v2_v := vector_inj v2 size.+1 in 
+  rev (vector_sub v1 v2) = 
+  vec_to_list_float size.+1 (\col_j (v1_v +f (-f v2_v)) j ord0).
+Proof.
+intros.
+apply nth_ext with 0%F32 0%F32.
++ admit.
++ intros. simpl.  
+  induction n.
+  - simpl. rewrite !mxE /=.
+    unfold vector_sub, map2, uncurry.
+    (*assert (0%F32 = (fun p : ftype Tsingle * ftype Tsingle
+                         => let (x, y) := p in (x - y)%F32)
+                     (Zconst Tsingle 0, Zconst Tsingle 0)).
+    { admit. } once rewrite H1.
+    Print map_nth. 
+*)
+    rewrite (@map_nth _ _ _ _ (Zconst Tsingle 0, Zconst Tsingle 0) _).
+    rewrite combine_nth.
+    admit.
+    admit.
+  - simpl.
+
+
+
+
+Print map.
+apply (nth_ext.
+
+remember (combine v1 v2) as L.
+induction L.
++ by simpl in H.  
++ simpl. rewrite IHL. rewrite /v1_v /v2_v.
+  assert (size.+1 = (length L).-1.+1).
+  { rewrite /size. simpl.
+
+
+
+
+ destruct L.
++ by simpl in H.
++ 
+
+
+
+
+
 Lemma func_model_equiv (A: matrix Tsingle) (b: vector Tsingle) (x: vector Tsingle) (n: nat) :
   let size := (length A).-1 in  
   let x_v := vector_inj x size.+1 in 
@@ -104,8 +165,27 @@ induction n.
   intros. by rewrite !mxE /=.  
 + simpl. rewrite -IHn.
   apply /matrixP. unfold eqrel.
-  intros. rewrite !mxE. 
+  move=> i j.
+  rewrite !mxE. 
   unfold jacob_list_fun_model.jacobi_iter.
+  remember (jacobi_n A b x n) as x_n.
+  assert ((vector_sub b
+           (matrix_vector_mult (remove_diag A) x_n))  = 
+          (vec_to_list_float size.+1
+              (\col_j0 (b_v +f
+                        -f (A2_J A_v *f vector_inj x_n size.+1)) j0 j))).
+  { unfold vector_sub.
+
+
+
+  remember  (combine
+     (vec_to_list_float size.+1 (\row_j0 A1_inv_J A_v i j0)^T)
+     (vec_to_list_float size.+1
+        (\col_j0 (b_v +f -f
+                  (A2_J A_v *f vector_inj x_n size.+1)) j0 j))) as L.
+  unfold diagmatrix_vector_mult.
+
+
   Print diagmatrix_vector_mult.
   Print dot_prodF.
 
