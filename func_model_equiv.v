870ef8db18db26c2b481455dea34a7e172a0a8b7
Mohit Tekriwal <tmohit@umich.edu>
2023-01-04 10:36:00 +0100

    added equivalency between mathcomp and list-list defintions

---
+++ func_model_equiv.v
@@ -0,0 +1,96 @@
+From Coq Require Import ZArith Reals Psatz.
+From Flocq Require Import Binary.
+From mathcomp Require Import all_ssreflect ssralg ssrnat all_algebra seq matrix.
+
+
+Import List ListNotations.
+
+From vcfloat Require Import FPLang FPLangOpt RAux Rounding Reify 
+                            Float_notations Automate.
+Require Import dot_prod_defn float_model_generic.
+Require Import floatlib.
+Set Bullet Behavior "Strict Subproofs". 
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Section WITHNANS.
+Context {NANS: Nans}. 
+
+
+Notation "A +f B" := (addmx_float A B) (at level 80).
+Notation "-f A" := (opp_mat A) (at level 50).
+Notation "A *f B" := (mulmx_float A B) (at level 70).
+
+
+Definition jacobi_iter {n:nat} x0 b (A: 'M[ftype Tsingle]_n.+1) : 
+  'cV[ftype Tsingle]_n.+1 :=
+   let r := b +f (-f ((A2_J A) *f x0)) in
+   (A1_inv_J A) *f r.
+
+
+Definition X_m_jacobi {n:nat} m x0 b (A: 'M[ftype Tsingle]_n.+1) :
+  'cV[ftype Tsingle]_n.+1 :=
+   Nat.iter m  (fun x0 => jacobi_iter x0 b A) x0.
+
+
+(** Define a relation between the list list definition of matrix
+    and mathcomp definition of matrix
+**)
+
+Print List.nth.
+
+
+
+Definition matrix_inj {t} (A: matrix t)  : 'M[ftype t]_(length A):=
+    \matrix_(i < (length A), j < (length A)) 
+     nth j (nth i A [::]) (Zconst t 0).
+
+
+(** Check **)
+Definition A_check : (matrix Tsingle) :=  
+  [[1%F32 ; 2%F32]; [3%F32 ; 4%F32]].
+
+Check A_check.
+
+Definition A_1 : 'M[ftype Tsingle]_2 :=
+  \matrix_(i < 2, j < 2)
+   if (i == 0%N :> nat) then
+    (if (j == 0%N :> nat) then 1%F32 else 2%F32) else
+    (if (j == 0%N :> nat) then 3%F32 else 4%F32).
+
+(*
+Lemma matrix_equiv_check:
+  matrix_inj A_check = A_1.
+Proof.
+apply /matrixP. unfold eqrel. 
+assert (length A_check = 2%nat).
+{ by []. } 
+intros. rewrite !mxE //=.
+case: x. intros.
++ simpl. destruct m.
+  - simpl.
+    case: y. intros.
+    simpl. destruct m.
+    * by simpl.
+    * simpl. destruct m.
+      ++ by [].
+      ++ rewrite H in i0. admit.
+ - simpl. 
+   case: y. intros.
+   simpl. destruct m0.
+   * simpl. destruct m.
+     ++ by simpl.
+     ++ admit.
+   * simpl. destruct m.
+     ++ simpl.
+ *)
+
+
+Lemma func_model_equiv:
+  @jacobi_n Tsingle 
+
+
+
+End WITHNANS.
