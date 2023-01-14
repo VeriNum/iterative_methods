543a251bd47f13c08f0766e5a0ecc0c08e3de1f6
Mohit Tekriwal <tmohit@umich.edu>
2023-01-14 10:08:43 +0100

    Defining error bound for matrix vector multiplication from fma dot product

--- fma_matrix_vec_mult.v
+++ fma_matrix_vec_mult.v
@@ -1,21 +1,30 @@
 From Coq Require Import ZArith Reals Psatz.
 From Flocq Require Import Binary.
 From mathcomp Require Import all_ssreflect ssralg ssrnat all_algebra seq matrix.
+From mathcomp.analysis Require Import Rstruct.
 Import List ListNotations.
 
 
 From vcfloat Require Import FPLang FPLangOpt RAux Rounding Reify 
                             Float_notations Automate.
 (*Require Import float_model_generic. *)
-Require Import floatlib jacob_list_fun_model fma_dot_mat_model.
+Require Import floatlib jacob_list_fun_model fma_dot_mat_model inf_norm_properties.
 
-Require Import common fma_dot_acc float_acc_lems.
+Require Import common fma_dot_acc float_acc_lems dotprod_model.
 
 
 Set Bullet Behavior "Strict Subproofs". 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
+
+
+Open Scope ring_scope.
+
+Delimit Scope ring_scope with Ri.
+Delimit Scope R_scope with Re.
+
+Import Order.TTheory GRing.Theory Num.Def Num.Theory.
 
 
 Section WITHNANS.
@@ -27,6 +36,22 @@ Notation "A *f B" := (mulmx_float A B) (at level 70).
 Notation "A -f B" := (sub_mat A B) (at level 80).
 
 
+
+Definition e_i {n:nat} {ty} (i : 'I_n.+1) 
+  (A: 'M[ftype ty]_n.+1) (v: 'cV[ftype ty]_n.+1) := 
+  let l1 := vec_to_list_float n.+1 (\row_(j < n.+1) A i j)^T in
+  let l2 := vec_to_list_float n.+1 v in
+  let L := combine l1 l2 in
+  let prods := map (uncurry Rmult) (map Rabsp (map FR2 L)) in
+  let rs:= sum_fold prods in
+  (g ty (length l1) * rs  + g1 ty (length l1) (length l1 - 1))%Re.
+
+
+Definition mat_vec_mult_err_bnd {n:nat} {ty}
+ (A: 'M[ftype ty]_n.+1) (v: 'cV[ftype ty]_n.+1):=
+ bigmaxr 0%Re [seq (e_i i A v) | i <- enum 'I_n.+1].
+
+
 Definition FT2R_mat {m n: nat} {ty} (A : 'M[ftype ty]_(m.+1, n.+1)) :
    'M[R]_(m.+1, n.+1):=
   \matrix_(i, j) FT2R (A i j).
@@ -36,7 +61,7 @@ Definition FT2R_mat {m n: nat} {ty} (A : 'M[ftype ty]_(m.+1, n.+1)) :
 Lemma matrix_vec_mult_bound {n:nat} {ty}:
   forall (A: 'M[ftype ty]_n.+1) (v : 'cV[ftype ty]_n.+1),
   vec_inf_norm (FT2R_mat (A *f v) - (FT2R_mat A) *m (FT2R_mat v)) <=
-   
+  mat_vec_mult_err_bnd A v.
 
 
 
