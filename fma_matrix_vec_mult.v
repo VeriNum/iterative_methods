2f322c2b87106b3f4879da7ecb0275eaf6e26103
Mohit Tekriwal <tmohit@umich.edu>
2023-01-16 11:15:09 +0100

    updated

--- fma_matrix_vec_mult.v
+++ fma_matrix_vec_mult.v
@@ -414,7 +414,27 @@ apply Rle_trans with (Rabs 1 + Rabs delta)%Re.
   apply Hd.
 Qed.
 
-  
+Lemma BPLUS_error_le_rel
+  {NAN: Nans} (t : type) :
+  forall x y 
+  (FIN: Binary.is_finite _ _ (BPLUS t x y) = true),
+  Rabs (FT2R (BPLUS t x y ) - (FT2R x + FT2R y)) <= (Rabs (FT2R x) + Rabs (FT2R y)) * (default_rel t).
+Proof.
+intros.
+pose proof (BPLUS_accurate' t x y FIN).
+destruct H as [delta H].
+destruct H as [Hd Heq].
+rewrite Heq.
+assert (((FT2R x + FT2R y) * (1 + delta) - (FT2R x + FT2R y))%Re =
+        ((FT2R x + FT2R y) * delta)%Re).
+{ nra. } rewrite H.
+rewrite Rabs_mult.
+apply /RleP. rewrite -RmultE -RplusE.
+apply Rmult_le_compat; try by apply Rabs_pos.
++ apply Rabs_triang.
++ apply Hd.
+Qed.
+
 
 
 Lemma vec_float_sub {ty} {n:nat} (v1 v2 : 'cV[ftype ty]_n.+1):
@@ -428,6 +448,38 @@ intros. rewrite seq_equiv.
 rewrite nth_mkseq; last by rewrite size_map size_enum_ord in H.
 rewrite !mxE. rewrite -!RminusE -RmultE -!RplusE.
 rewrite Bminus_bplus_opp_equiv.
++ assert ((FT2R (v1 (inord i) ord0) -  FT2R (v2 (inord i) ord0))%Re = 
+          (FT2R (v1 (inord i) ord0) +  FT2R (BOPP ty (v2 (inord i) ord0)))%Re ).
+  { unfold FT2R. rewrite B2R_Bopp. nra. }
+  rewrite H0.
+  apply Rle_trans with 
+  ((Rabs (FT2R (v1 (inord i) ord0)) + Rabs (FT2R (BOPP ty (v2 (inord i) ord0)))) * (default_rel ty))%Re.
+  - apply /RleP. apply BPLUS_error_le_rel. admit.
+  - apply Rle_trans with
+    ((bigmaxr 0%Re
+          [seq Rabs (FT2R_mat v1 i0 ord0)
+             | i0 <- enum 'I_n.+1] +
+        bigmaxr 0%Re
+          [seq Rabs (FT2R_mat v2 i0 ord0)
+             | i0 <- enum 'I_n.+1]) * default_rel ty)%Re.
+    *
+
+
+
+ admit.
+    * 
+
+
+rewrite Rmult_plus_distr_r.
+    apply Rle_trans with 
+    
+
+    apply Rplus_le_compat.
+
+
+Search ((_ + _ ) * _ )%Re.
+  
+
 
 rewrite BPLUS_accurate'.
 
