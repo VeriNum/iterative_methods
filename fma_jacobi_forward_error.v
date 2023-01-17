b44f6d065226f2cc24fae61c79a13e003e8fac98
Mohit Tekriwal <tmohit@umich.edu>
2023-01-17 11:29:29 +0100

    Stated the theorem for forward error bound

--- fma_jacobi_forward_error.v
+++ fma_jacobi_forward_error.v
@@ -12,6 +12,7 @@ Require Import floatlib jacob_list_fun_model fma_dot_mat_model
                inf_norm_properties fma_matrix_vec_mult.
 
 Require Import common fma_dot_acc float_acc_lems dotprod_model.
+Require Import fma_matrix_vec_mult vec_sum_inf_norm_rel.
 
 
 Set Bullet Behavior "Strict Subproofs". 
@@ -40,18 +41,6 @@ Notation "A -f B" := (sub_mat A B) (at level 80).
 
 (** Define forward error **)
 
-(**
-Definition diag_vector_mult {ty} {n:nat} (v1 v2: 'cV[ftype ty]_n.+1)
-  : 'cV[ftype ty]_n.+1 :=
-  \col_i (BMULT ty (nth (n.+1.-1 -i) (vec_to_list_float n.+1 v1) (Zconst ty 0))
-            (nth (n.+1.-1 - i) (vec_to_list_float n.+1 v2) (Zconst ty 0))).
-
-Definition jacobi_iter {ty} {n:nat} x0 b (A: 'M[ftype ty]_n.+1) : 
-  'cV[ftype ty]_n.+1 :=
-   let r := b -f ((A2_J A) *f x0) in
-   diag_vector_mult (A1_inv_J A) r.
-**)
-
 Fixpoint vec_to_list_real {n:nat} (m:nat) (v :'cV[R]_n.+1)
    : list R := 
    match m with 
@@ -79,13 +68,45 @@ Definition x_fix {n:nat} x b (A: 'M[R]_n.+1) :
   let r := b - ((A2_J_real A) *m x) in
   diag_matrix_vec_mult_R (A1_diag A) r.
 
-Definition f_error {ty} {n:nat} m b x0 x (A: 'M[ftype ty]_n.+1)
-  : 'cV[R]_n.+1:=
+Definition f_error {ty} {n:nat} m b x0 x (A: 'M[ftype ty]_n.+1):=
   let x_k := X_m_jacobi m x0 b A in 
   let A_real := FT2R_mat A in
   let b_real := FT2R_mat b in
   let x := x_fix x b_real A_real in
-  FT2R_mat x_k - x.
+  vec_inf_norm (FT2R_mat x_k - x).
+
+(** State the forward error theorem **)
+Theorem jacobi_forward_error_bound {ty} {n:nat} 
+  (A: 'M[ftype ty]_n.+1) (b: 'cV[ftype ty]_n.+1):
+  let A_real := FT2R_mat A in
+  let b_real := FT2R_mat b in
+  let x:= A_real^-1 *m b_real in
+  x != 0 ->
+  (forall (A: 'M[ftype ty]_n.+1) (v: 'cV[ftype ty]_n.+1)
+          (xy : ftype ty * ftype ty) (i : 'I_n.+1),
+    In xy
+      (combine
+         (vec_to_list_float n.+1
+            (\row_j A (inord i) j)^T)
+         (vec_to_list_float n.+1 v)) ->
+    is_finite (fprec ty) (femax ty) xy.1 = true /\
+    is_finite (fprec ty) (femax ty) xy.2 = true /\ 
+      Rabs (FT2R (fst (xy))) <= sqrt (F' ty / (INR n.+1 * (1 + default_rel ty)^n.+1) - default_abs ty) /\
+      Rabs (FT2R (snd (xy))) <= sqrt (F' ty / (INR n.+1 * (1 + default_rel ty)^n.+1) - default_abs ty)) ->
+  
+  exists rho d_mag,
+  forall x0: 'cV[ftype ty]_n.+1, 
+  forall k:nat,
+  (f_error k b x0 x A <= rho^k * (f_error 0 b x0 x A) + ((1 - rho^k) / (1 - rho))* d_mag)%Re.
+Admitted. 
+  
+
+
+
+
+
+
+
 
 
 
