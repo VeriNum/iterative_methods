b6cf1fdd91d3e9967d3f5f19ece11178b2065df5
Mohit Tekriwal <tmohit@umich.edu>
2023-01-21 22:39:24 +0100

    added definition for jacobi_iteration_bound

--- jacob_list_fun_model.v
+++ jacob_list_fun_model.v
@@ -1,7 +1,10 @@
 Require Import vcfloat.VCFloat.
-From Iterative Require Import floatlib.
+From mathcomp Require Import all_ssreflect ssralg  ssrnat all_algebra seq matrix mxalgebra.
+
+Require Import floatlib.
 Require Import Coq.Lists.List. Import ListNotations.
 Set Bullet Behavior "Strict Subproofs".
+
 
 Section WITH_NANS.
 
@@ -61,18 +64,121 @@ Definition jacobi {t: type} (A: matrix t) (b: vector t) (x: vector t) (acc: ftyp
          ftype t * vector t :=
    let A1 := diag_of_matrix  A in 
    let A2 := remove_diag A in 
-   iter_stop norm2 (jacobi_residual A1 A2 b) (jacobi_iter A1 A2 b) (pred n) acc x.
+   iter_stop norm2 (jacobi_residual A1 A2 b) (jacobi_iter A1 A2 b) (Nat.pred n) acc x.
 
 Definition old_jacobi_iter {t: type} x0 b (A1: diagmatrix t) (A2: matrix t) : vector t :=
   let S_J :=  opp_matrix (diagmatrix_matrix_mult A1 A2) in
   let f_J := diagmatrix_vector_mult A1 b in
   vector_add (matrix_vector_mult S_J x0) f_J.
 
+Definition matrix_inj {t} (A: matrix t) m n  : 'M[ftype t]_(m,n):=
+    \matrix_(i < m, j < n) 
+     nth j (nth i A [::]) (Zconst t 0).
+
+
+Definition vector_inj {t} (v: vector t) n  : 'cV[ftype t]_n :=
+   \col_(i < n) nth i v (Zconst t 0).
+
+Definition FT2R_mat {m n: nat} {ty} (A : 'M[ftype ty]_(m.+1, n.+1)) :
+   'M[R]_(m.+1, n.+1):=
+  \matrix_(i, j) FT2R (A i j).
+
+Definition A1_diag {n: nat} (A: 'M[R]_n.+1) : 'cV[R]_n.+1:=
+  \col_i (1 / (A i i)).
+
+Definition A2_J_real {n:nat} (A: 'M[R]_n.+1): 
+  'M[R]_n.+1 :=
+  \matrix_(i,j) 
+    if (i==j :> nat) then 0 else A i j. 
+
+Fixpoint vec_to_list_real {n:nat} (m:nat) (v :'cV[R]_n.+1)
+   : list R := 
+   match m with 
+   | O => []
+   | S p => [v (@inord n p) ord0] ++ vec_to_list_real p v
+   end.
+
+Definition diag_matrix_vec_mult_R {n:nat} (v1 v2 : 'cV[R]_n.+1)
+  : 'cV[R]_n.+1 :=
+  \col_i ((nth (n.+1.-1 -i) (vec_to_list_real n.+1 v1) 0) * 
+          (nth (n.+1.-1 -i) (vec_to_list_real n.+1 v2) 0)).
+
+Definition x_fix {n:nat} x b (A: 'M[R]_n.+1) : 
+  'cV[R]_n.+1 :=
+  let r := b - (@mulmx _ n.+1 n.+1 _ (A2_J_real A) x) in
+  diag_matrix_vec_mult_R (A1_diag A) r.
+
+Require Import inf_norm_properties common.
 Definition jacobi_iteration_bound {t: type}
   (A: matrix t) (b: vector t) (accuracy: ftype t) (k: nat) : Prop :=
   (* some property of A,b,accuracy holds such that 
     jacobi_n will indeed converge within k iterations to this accuracy, 
    without ever overflowing *)
+  let A' := matrix_inj A in
+  let b' := vector_inj b in
+  let A_real := FT2R_mat A' in
+  let b_real := FT2R_mat b' in
+  let n := length A in
+  let x:= mulmx (A_real^-1) b_real in
+  let R := (vec_inf_norm (A1_diag A_real) * matrix_inf_norm (A2_J_real A_real)) in
+  let delta := default_rel t in
+  let rho := ((((1 + g t n.+1) * (1 + delta) *
+                  g t n.+1 + delta * (1 + g t n.+1) +
+                  g t n.+1) * (1 + delta) + delta) * R +
+                (((1 + g t n.+1) * (1 + delta) *
+                  g t n.+1 + delta * (1 + g t n.+1) +
+                  g t n.+1) * default_abs t +
+                 default_abs t) *
+                matrix_inf_norm (A2_J_real A_real) + R) in
+   let d_mag := ((g t n.+1 * (1 + delta) + delta) *
+                    ((vec_inf_norm (A1_diag A_real) *
+                      (1 + delta) + default_abs t) *
+                     vec_inf_norm b_real) +
+                    (1 + g t n.+1) * g1 t n.+1 (n.+1 - 1) *
+                    (1 + delta) *
+                    (vec_inf_norm (A1_diag A_real) *
+                     (1 + delta) + default_abs t) +
+                    g1 t n.+1 (n.+1 - 1) +
+                    (vec_inf_norm (A1_diag A_real) * delta +
+                     default_abs t) * vec_inf_norm b_real +
+                    ((((1 + g t n.+1) * (1 + delta) *
+                       g t n.+1 + delta * (1 + g t n.+1) +
+                       g t n.+1) * (1 + delta) + delta) * R +
+                     (((1 + g t n.+1) * (1 + delta) *
+                       g t n.+1 + delta * (1 + g t n.+1) +
+                       g t n.+1) * default_abs t +
+                      default_abs t) *
+                     matrix_inf_norm (A2_J_real A_real)) *
+                    vec_inf_norm (x_fix x b_real A_real)) in
+  
+  (forall i, is_finite _ _ (A' i i) = true) /\
+  x != 0 /\
+  (rho < 1)%Re /\
+  A_real \in unitmx /\
+  (forall i : 'I_n.+1,
+    is_finite (fprec ty) (femax ty)
+      (BDIV ty (Zconst ty 1) (A' i i)) = true) /\
+  (forall x0: 'cV[ftype ty]_n.+1, 
+    (forall k:nat, 
+      forall i, is_finite _ _ ((X_m_jacobi k x0 b A') i ord0) = true) /\
+    (forall k:nat,
+      (f_error k b x0 x A <= rho^k * (f_error 0 b x0 x A) + ((1 - rho^k) / (1 - rho))* d_mag))).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   False.  (* need to fill this in! *)
 
 Lemma jacobi_iteration_bound_monotone:
