17ce62d975e9a6adae156fd48abe39f6d687cd48
Mohit Tekriwal <tmohit@umich.edu>
2023-01-21 20:16:45 +0100

    correcting for error from division in the functional model

--- fma_jacobi_forward_error.v
+++ fma_jacobi_forward_error.v
@@ -830,8 +830,14 @@ Theorem jacobi_forward_error_bound {ty} {n:nat}
   x != 0 ->
    let R := (vec_inf_norm (A1_diag A_real) * matrix_inf_norm (A2_J_real A_real))%Re in
    let delta := default_rel ty in
-   let rho := (((1 + g ty n.+1) * (1 + delta) * g ty n.+1 +
-                delta * (1 + g ty n.+1) + g ty n.+1 + 1) * R)%Re in
+   let rho := ((((1 + g ty n.+1) * (1 + delta) *
+                  g ty n.+1 + delta * (1 + g ty n.+1) +
+                  g ty n.+1) * (1 + delta) + delta) * R +
+                (((1 + g ty n.+1) * (1 + delta) *
+                  g ty n.+1 + delta * (1 + g ty n.+1) +
+                  g ty n.+1) * default_abs ty +
+                 default_abs ty) *
+                matrix_inf_norm (A2_J_real A_real) + R)%Re in
    let d_mag := (((1 + g ty n.+1) * (1 + delta) * g ty n.+1 +
                    delta * (1 + g ty n.+1) + g ty n.+1) *
                   (R * vec_inf_norm (x_fix x b_real A_real)) +
@@ -1707,66 +1713,82 @@ induction k.
                                     apply Rmult_le_pos. apply default_rel_ge_0.
                                     apply Rplus_le_le_0_compat; try nra; try apply g_pos.
                         ***** apply H7.
+                    ---- assert ( (((((1 + g ty n.+1) * (1 + default_rel ty) *
+                                    g ty n.+1 +
+                                    default_rel ty * (1 + g ty n.+1) +
+                                    g ty n.+1) * (1 + default_rel ty) +
+                                   default_rel ty) * R2 +
+                                  (((1 + g ty n.+1) * (1 + default_rel ty) *
+                                    g ty n.+1 +
+                                    default_rel ty * (1 + g ty n.+1) +
+                                    g ty n.+1) * default_abs ty +
+                                   default_abs ty) *
+                                  matrix_inf_norm (A2_J_real A_real)) *
+                                 (f_error k b x0 x A +
+                                  vec_inf_norm (x_fix x b_real A_real)) +
+                                 ((g ty n.+1 * (1 + default_rel ty) +
+                                   default_rel ty) *
+                                  ((vec_inf_norm (A1_diag A_real) *
+                                    (1 + default_rel ty) + default_abs ty) *
+                                   vec_inf_norm b_real) +
+                                  (1 + g ty n.+1) * g1 ty n.+1 (n.+1 - 1) *
+                                  (1 + default_rel ty) *
+                                  (vec_inf_norm (A1_diag A_real) *
+                                   (1 + default_rel ty) + default_abs ty) +
+                                  g1 ty n.+1 (n.+1 - 1) +
+                                  (vec_inf_norm (A1_diag A_real) *
+                                   default_rel ty + default_abs ty) *
+                                  vec_inf_norm b_real) +
+                                 R2 * f_error k b x0 x A)%Re = 
+                                ((( ((((1 + g ty n.+1) * (1 + default_rel ty) *
+                                    g ty n.+1 +
+                                    default_rel ty * (1 + g ty n.+1) +
+                                    g ty n.+1) * (1 + default_rel ty) +
+                                   default_rel ty) * R2 +
+                                  (((1 + g ty n.+1) * (1 + default_rel ty) *
+                                    g ty n.+1 +
+                                    default_rel ty * (1 + g ty n.+1) +
+                                    g ty n.+1) * default_abs ty +
+                                   default_abs ty) *
+                                  matrix_inf_norm (A2_J_real A_real)) + R2) *
+                                  f_error k b x0 x A) + 
+                                (((g ty n.+1 * (1 + default_rel ty) +
+                                   default_rel ty) *
+                                  ((vec_inf_norm (A1_diag A_real) *
+                                    (1 + default_rel ty) + default_abs ty) *
+                                   vec_inf_norm b_real) +
+                                  (1 + g ty n.+1) * g1 ty n.+1 (n.+1 - 1) *
+                                  (1 + default_rel ty) *
+                                  (vec_inf_norm (A1_diag A_real) *
+                                   (1 + default_rel ty) + default_abs ty) +
+                                  g1 ty n.+1 (n.+1 - 1) +
+                                  (vec_inf_norm (A1_diag A_real) *
+                                   default_rel ty + default_abs ty) *
+                                  vec_inf_norm b_real) + 
+                                  ((((1 + g ty n.+1) * (1 + default_rel ty) *
+                                    g ty n.+1 +
+                                    default_rel ty * (1 + g ty n.+1) +
+                                    g ty n.+1) * (1 + default_rel ty) +
+                                   default_rel ty) * R2 +
+                                  (((1 + g ty n.+1) * (1 + default_rel ty) *
+                                    g ty n.+1 +
+                                    default_rel ty * (1 + g ty n.+1) +
+                                    g ty n.+1) * default_abs ty +
+                                   default_abs ty) *
+                                  matrix_inf_norm (A2_J_real A_real)) * 
+                                  vec_inf_norm (x_fix x b_real A_real)))%Re).
+                              { nra. }  rewrite H8. clear H8. fold delta. fold rho. fold d_mag.
+                              unfold f_error. fold b_real. fold A_real.
 
 
 
 
 
 
-                        ***** apply Rmult_le_pos.
-                            +++++ apply Rplus_le_le_0_compat.
-                                 ---- apply Rplus_le_le_0_compat.
-                                      **** repeat apply Rmult_le_pos.
-                                           +++++ apply Rplus_le_le_0_compat. nra. apply g_pos.
-                                           +++++ apply Rplus_le_le_0_compat. nra. apply default_rel_ge_0.
-                                           +++++ apply g_pos.
-                                      **** apply Rmult_le_pos.
-                                           +++++ apply default_rel_ge_0.
-                                           +++++ apply Rplus_le_le_0_compat. nra. apply g_pos.
-                                 ---- apply g_pos.
-                            ++++ unfold R2. apply Rmult_le_pos.
-                                 ---- apply /RleP. apply vec_norm_pd.
-                                 ---- apply /RleP. apply matrix_norm_pd.
-                        *** apply H6.
 
 
 
-
-
-
-
-
-                    apply Rle_trans with
-                    (((1 + g ty n.+1) * (1 + default_rel ty) * g ty n.+1 +
-                        default_rel ty * (1 + g ty n.+1) + 
-                        g ty n.+1) * R2 *
-                       (f_error k b x0 x A +
-                            vec_inf_norm (x_fix x b_real A_real)) +
-                       ((g ty n.+1 * (1 + default_rel ty) + default_rel ty) *
-                        (vec_inf_norm (A1_diag A_real) *
-                         vec_inf_norm b_real) +
-                        (1 + g ty n.+1) * g1 ty n.+1 (n.+1 - 1) *
-                        (1 + default_rel ty) *
-                        vec_inf_norm (A1_diag A_real)) +
-                       g1 ty n.+1 (n.+1 - 1) + R2 * f_error k b x0 x A )%Re.
-                    --- repeat apply Rplus_le_compat_r.
-                        repeat apply Rmult_le_compat_l.
-                        *** apply Rmult_le_pos.
-                            ++++ apply Rplus_le_le_0_compat.
-                                 ---- apply Rplus_le_le_0_compat.
-                                      **** repeat apply Rmult_le_pos.
-                                           +++++ apply Rplus_le_le_0_compat. nra. apply g_pos.
-                                           +++++ apply Rplus_le_le_0_compat. nra. apply default_rel_ge_0.
-                                           +++++ apply g_pos.
-                                      **** apply Rmult_le_pos.
-                                           +++++ apply default_rel_ge_0.
-                                           +++++ apply Rplus_le_le_0_compat. nra. apply g_pos.
-                                 ---- apply g_pos.
-                            ++++ unfold R2. apply Rmult_le_pos.
-                                 ---- apply /RleP. apply vec_norm_pd.
-                                 ---- apply /RleP. apply matrix_norm_pd.
-                        *** apply H6.
-                    --- assert ((((1 + g ty n.+1) * (1 + default_rel ty) * g ty n.+1 +
+assert ((((1 + g ty n.+1) * (1 + default_rel ty) * g ty n.+1 +
                                   default_rel ty * (1 + g ty n.+1) + 
                                   g ty n.+1) * R2 *
                                  (f_error k b x0 x A +
