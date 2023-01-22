6141327b4bac8078dc9b874c8b594ead6a141b55
Mohit Tekriwal <tmohit@umich.edu>
2023-01-22 11:12:18 +0100

    updated the definition

--- jacob_list_fun_model.v
+++ jacob_list_fun_model.v
@@ -1,10 +1,11 @@
 Require Import vcfloat.VCFloat.
-From mathcomp Require Import all_ssreflect ssralg  ssrnat all_algebra seq matrix mxalgebra.
+From mathcomp Require Import all_ssreflect ssralg  ssrnat all_algebra seq matrix .
+From mathcomp.analysis Require Import Rstruct.
 
 Require Import floatlib.
 Require Import Coq.Lists.List. Import ListNotations.
 Set Bullet Behavior "Strict Subproofs".
-
+Require Import fma_floating_point_model.
 
 Section WITH_NANS.
 
@@ -71,61 +72,22 @@ Definition old_jacobi_iter {t: type} x0 b (A1: diagmatrix t) (A2: matrix t) : ve
   let f_J := diagmatrix_vector_mult A1 b in
   vector_add (matrix_vector_mult S_J x0) f_J.
 
-(** Adding some definitions from other files just for now **)
-
-Definition matrix_inj {t} (A: matrix t) m n  : 'M[ftype t]_(m,n):=
-    \matrix_(i < m, j < n) 
-     nth j (nth i A [::]) (Zconst t 0).
-
-
-Definition vector_inj {t} (v: vector t) n  : 'cV[ftype t]_n :=
-   \col_(i < n) nth i v (Zconst t 0).
-
-Definition FT2R_mat {m n: nat} {ty} (A : 'M[ftype ty]_(m.+1, n.+1)) :
-   'M[R]_(m.+1, n.+1):=
-  \matrix_(i, j) FT2R (A i j).
-
-Definition A1_diag {n: nat} (A: 'M[R]_n.+1) : 'cV[R]_n.+1:=
-  \col_i (1 / (A i i)).
-
-Definition A2_J_real {n:nat} (A: 'M[R]_n.+1): 
-  'M[R]_n.+1 :=
-  \matrix_(i,j) 
-    if (i==j :> nat) then 0 else A i j. 
-
-Fixpoint vec_to_list_real {n:nat} (m:nat) (v :'cV[R]_n.+1)
-   : list R := 
-   match m with 
-   | O => []
-   | S p => [v (@inord n p) ord0] ++ vec_to_list_real p v
-   end.
-
-Definition diag_matrix_vec_mult_R {n:nat} (v1 v2 : 'cV[R]_n.+1)
-  : 'cV[R]_n.+1 :=
-  \col_i ((nth (n.+1.-1 -i) (vec_to_list_real n.+1 v1) 0) * 
-          (nth (n.+1.-1 -i) (vec_to_list_real n.+1 v2) 0)).
-
-Import Order.TTheory GRing.Theory Num.Def Num.Theory.
-
-(** Dummy x_fix. there are some issues with using the real defintion 
-    in this file
-**)
-Definition x_fix {n:nat} (x: 'cV[R]_n.+1) b (A: 'M[R]_n.+1) : 
-  'cV[R]_n.+1 := 
-  let r :=  b  in
-  diag_matrix_vec_mult_R (A1_diag A) r.
+Require Import fma_real_func_model fma_floating_point_model.
 
 Require Import inf_norm_properties common.
+
+
 Definition jacobi_iteration_bound {t: type}
   (A: matrix t) (b: vector t) (accuracy: ftype t) (k: nat) : Prop :=
   (* some property of A,b,accuracy holds such that 
     jacobi_n will indeed converge within k iterations to this accuracy, 
    without ever overflowing *)
-  let A' := matrix_inj A in
-  let b' := vector_inj b in
+  let n := (length A).-1 in
+  let A' := @matrix_inj _ A n.+1 n.+1 in
+  let b' := @vector_inj _ b n.+1 in
   let A_real := FT2R_mat A' in
   let b_real := FT2R_mat b' in
-  let n := length A in
+  
   let x:= mulmx (A_real^-1) b_real in
   let R := (vec_inf_norm (A1_diag A_real) * matrix_inf_norm (A2_J_real A_real)) in
   let delta := default_rel t in
@@ -167,26 +129,12 @@ Definition jacobi_iteration_bound {t: type}
       (BDIV t (Zconst t 1) (A' i i)) = true) /\
   (forall x0: 'cV[ftype t]_n.+1, 
     (forall k:nat, 
-      forall i, Binary.is_finite _ _ ((X_m_jacobi k x0 b A') i ord0) = true) /\
-    (forall k:nat,
-      (f_error k b x0 x A <= rho^k * (f_error 0 b x0 x A) + ((1 - rho^k) / (1 - rho))* d_mag))).
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      forall i, Binary.is_finite _ _ ((X_m_jacobi k x0 b' A') i ord0) = true)).
+(*
 
 
   False.  (* need to fill this in! *)
+*)
 
 Lemma jacobi_iteration_bound_monotone:
   forall {t: type}  (A: matrix t) (b: vector t) (acc: ftype t) (k k': nat),
