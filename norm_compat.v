b5d4ed9efb3065052e1a90e3d75d764018112b65
Mohit Tekriwal <tmohit@umich.edu>
2023-01-23 23:36:48 +0100

    proved compatibility relationship between 2-norm and inf-norm

--- norm_compat.v
+++ norm_compat.v
@@ -22,6 +22,17 @@ Definition vec_norm2 {n:nat} (v : 'cV[R]_n.+1) :=
   sqrt (\sum_j (Rsqr (v j ord0))).
 
 
+Lemma sum_const {n:nat} (a:R): 
+  \sum_(j < n) a = (INR n * a)%Re.
+Proof.
+induction n.
++ rewrite big_ord0 /=. by rewrite RmultE mul0r.
++ rewrite big_ord_recr. rewrite IHn.
+  rewrite -addn1. rewrite plus_INR /=.
+  rewrite Rmult_plus_distr_r. rewrite -RplusE. nra.
+Qed. 
+
+
 Lemma norm2_inf_norm_compat {n:nat} (v : 'cV[R]_n.+1):
   (vec_norm2 v <= sqrt (INR n.+1) * vec_inf_norm v)%Re.
 Proof.
@@ -30,14 +41,30 @@ match goal with |-context[(_ <= _ * ?a)%Re]=>
   replace a with (sqrt (Rsqr a)) 
 end.
 + rewrite -sqrt_mult_alt.
-  - admit.
+  - apply sqrt_le_1_alt . rewrite -sum_const .
+    apply /RleP. apply big_sum_ge_ex_abstract.
+    intros. rewrite Rsqr_abs. apply  Rsqr_incr_1.
+    * apply Rle_trans with 
+      [seq Rabs (v i0 0) | i0 <- enum 'I_n.+1]`_i.
+      ++ rewrite seq_equiv. rewrite nth_mkseq. 
+         rewrite inord_val. apply Rle_refl.
+         apply ltn_ord.
+      ++ apply /RleP. 
+         apply (@bigmaxr_ler _ 0%Re 
+                    [seq Rabs (v i0 0) | i0 <- enum 'I_n.+1] i).
+    * rewrite size_map size_enum_ord. apply ltn_ord.
+    * apply Rabs_pos.
+  - apply /RleP. apply bigmax_le_0. apply /RleP. apply Rle_refl.
+    intros. rewrite seq_equiv. rewrite nth_mkseq;
+    last by rewrite size_map size_enum_ord in H0.
+    apply /RleP. apply Rabs_pos.
   - apply pos_INR.
 + apply sqrt_Rsqr. apply /RleP. apply bigmax_le_0.
   - apply /RleP. apply Rle_refl.
   - intros. rewrite seq_equiv. rewrite nth_mkseq;
     last by rewrite size_map size_enum_ord in H.
     apply /RleP. apply Rabs_pos.
-Admitted.
+Qed.
 
 
 
