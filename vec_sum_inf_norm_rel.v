0865a894e1bc1d3ac8e717fb725658999ee419db
Mohit Tekriwal <tmohit@umich.edu>
2023-01-27 15:31:40 +0100

    added a corollary lemma for vector subtraction

--- vec_sum_inf_norm_rel.v
+++ vec_sum_inf_norm_rel.v
@@ -103,6 +103,30 @@ apply Rmult_le_compat; try by apply Rabs_pos.
 Qed.
 
 
+Lemma BPLUS_error_le_rel'
+  {NAN: Nans} (t : type) :
+  forall x y 
+  (FIN: Binary.is_finite _ _ (BPLUS t x y) = true),
+  Rabs (FT2R (BPLUS t x y ) - (FT2R x + FT2R y)) <= (Rabs (FT2R x + FT2R y)) * (default_rel t).
+Proof.
+intros.
+pose proof (BPLUS_accurate' t x y FIN).
+destruct H as [delta H].
+destruct H as [Hd Heq].
+rewrite Heq.
+assert (((FT2R x + FT2R y) * (1 + delta) - (FT2R x + FT2R y))%Re =
+        ((FT2R x + FT2R y) * delta)%Re).
+{ nra. } rewrite H.
+rewrite Rabs_mult.
+apply /RleP. rewrite -RmultE.
+apply Rmult_le_compat; try by apply Rabs_pos.
++ nra.
++ apply Hd.
+Qed.
+
+
+
+
 Lemma Bplus_no_ov_is_finite : 
    forall (t: type) 
              x (FINx: Binary.is_finite (fprec t) (femax t) x = true) 
@@ -390,5 +414,84 @@ rewrite Bminus_bplus_opp_equiv.
 + rewrite is_finite_Bopp. apply Hfin.
 + by apply Hfin. 
 Qed.
+
+
+Lemma vec_float_sub_1 {ty} {n:nat} (v1 v2 : 'cV[ftype ty]_n.+1):
+  (forall (xy : ftype ty * ftype ty),
+    In xy
+      (combine 
+         (vec_to_list_float n.+1 v1)
+         (vec_to_list_float n.+1 v2)) ->
+    is_finite (fprec ty) (femax ty) xy.1 = true /\
+    is_finite (fprec ty) (femax ty) xy.2 = true /\ 
+    is_finite (fprec ty) (femax ty)
+        (BPLUS ty xy.1 (BOPP ty xy.2)) = true) ->
+  vec_inf_norm (FT2R_mat (v1 -f v2) - (FT2R_mat v1 - FT2R_mat v2)) <= 
+  (vec_inf_norm (FT2R_mat v1 - FT2R_mat v2)) * (default_rel ty) .
+Proof.
+intros Hfin.
+unfold vec_inf_norm.
+apply /RleP. apply bigmax_le; first by rewrite size_map size_enum_ord.
+intros. rewrite seq_equiv. 
+rewrite nth_mkseq; last by rewrite size_map size_enum_ord in H.
+rewrite !mxE. rewrite -!RminusE -RmultE.
+specialize (Hfin ((v1 (inord i) ord0), (v2 (inord i) ord0))).
+assert (Hin: In (v1 (inord i) ord0, v2 (inord i) ord0)
+           (combine (vec_to_list_float n.+1 v1)
+              (vec_to_list_float n.+1 v2))).
+{ apply in_rev. rewrite -combine_rev; last by rewrite !length_veclist.
+  assert ((v1 (inord i) ord0, v2 (inord i) ord0) = 
+           nth i (combine (rev (vec_to_list_float n.+1 v1))
+                    (rev (vec_to_list_float n.+1 v2))) (Zconst ty 0, Zconst ty 0)).
+  { rewrite combine_nth. rewrite !rev_nth !length_veclist.
+    assert ((n.+1 - i.+1)%coq_nat = (n.+1.-1 - i)%coq_nat).
+    { lia. } rewrite H0. rewrite !nth_vec_to_list_float; try by [].
+    by rewrite size_map size_enum_ord in H.
+    by rewrite size_map size_enum_ord in H.
+    apply /ssrnat.ltP. by rewrite size_map size_enum_ord in H.
+    apply /ssrnat.ltP. by rewrite size_map size_enum_ord in H.
+    by rewrite !rev_length !length_veclist.
+ } rewrite H0. apply nth_In. rewrite combine_length.
+ rewrite !rev_length !length_veclist Nat.min_id.
+ rewrite size_map size_enum_ord in H. by apply /ssrnat.ltP.
+} specialize (Hfin Hin).
+rewrite Bminus_bplus_opp_equiv.
++ assert ((FT2R (v1 (inord i) ord0) -  FT2R (v2 (inord i) ord0))%Re = 
+          (FT2R (v1 (inord i) ord0) +  FT2R (BOPP ty (v2 (inord i) ord0)))%Re ).
+  { unfold FT2R. rewrite B2R_Bopp. nra. }
+  rewrite H0.
+  apply Rle_trans with 
+  (Rabs (FT2R (v1 (inord i) ord0) + (FT2R (BOPP ty (v2 (inord i) ord0)))) * (default_rel ty))%Re.
+  - apply /RleP. apply BPLUS_error_le_rel'.
+    apply Hfin.
+  - apply Rmult_le_compat_r.
+    * apply default_rel_ge_0.
+    * apply Rle_trans with 
+      [seq Rabs
+          ((FT2R_mat v1 - FT2R_mat v2)%Ri i0
+             0)
+      | i0 <- enum 'I_n.+1]`_i.
+      ++ rewrite seq_equiv. rewrite nth_mkseq;
+         last by rewrite size_map size_enum_ord in H.
+         rewrite !mxE. rewrite -RminusE.
+         assert (FT2R (BOPP ty (v2 (inord i) ord0)) = 
+                  (- (FT2R (v2 (inord i) ord0)))%Re).
+         { unfold FT2R. rewrite B2R_Bopp. nra. }
+         rewrite H1. apply Rle_refl.
+      ++ apply /RleP. 
+         apply (@bigmaxr_ler _ 0%Re [seq Rabs
+                                           ((FT2R_mat v1 - FT2R_mat v2)%Ri i0
+                                              0)
+                                       | i0 <- enum 'I_n.+1] i).
+         rewrite size_map size_enum_ord.
+         by rewrite size_map size_enum_ord in H.
++ apply Hfin.
++ rewrite is_finite_Bopp. apply Hfin.
++ by apply Hfin. 
+Qed.
+
+
+
+
 
 End WITHNANS.
