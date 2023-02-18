ece0f72fcc14060d8fc6bb3cdf6a334964daae01
Mohit Tekriwal <tmohit@umich.edu>
2023-02-18 14:27:59 +0530

    working on jacobi_precond compute ==> jacobi_precond math

--- jacobi_iteration_bound.v
+++ jacobi_iteration_bound.v
@@ -414,6 +414,26 @@ repeat split.
   eapply Rle_lt_trans; last by apply bnd1.
   apply Rmult_le_compat_l. apply Rabs_pos.
   apply Rplus_le_compat.
+  - rewrite !Rmult_1_l. apply Rplus_le_compat.
+    * admit.
+    * apply Rmult_le_compat.
+      apply Rmult_le_pos. nra. apply d_mag_ge_0.
+      apply Rlt_le, Rinv_0_lt_compat. 
+      apply Rlt_Rminus. eapply Rle_lt_trans.
+      apply rho_def_le_alt. apply Hrho.
+      apply Rmult_le_compat_l. nra. apply d_mag_def_le_alt.
+      assert ((rho_def A b = rho_def_alt A b)%Re \/
+                  (rho_def A b < rho_def_alt A b)%Re).
+         { pose proof (@rho_def_le_alt t n A b). nra. }
+         destruct H. 
+         rewrite H; nra.
+         apply Rlt_le. apply Rinv_lt_contravar .
+         apply Rmult_lt_0_compat.
+         apply Rlt_Rminus. apply Hrho.
+         apply Rlt_Rminus. eapply Rle_lt_trans.
+         apply rho_def_le_alt. apply Hrho.
+         apply Rplus_le_lt_compat. nra.
+         by apply Ropp_lt_contravar.
   - 
 
 
