f3c97ada36a1630d3e96f682d81cdbf493bcbb1f
Mohit Tekriwal <tmohit@umich.edu>
2023-02-18 21:37:25 +0530

    updated

--- jacobi_iteration_bound.v
+++ jacobi_iteration_bound.v
@@ -291,12 +291,10 @@ apply Rplus_le_compat.
       ++ apply Rplus_le_le_0_compat; try apply default_rel_ge_0.
          apply Rmult_le_pos. apply g_pos.
          apply Rplus_le_le_0_compat; try nra; try apply default_rel_ge_0.
-      ++
-
-
-
-
- admit.
+      ++ apply Rmult_le_compat_r. apply /RleP. apply vec_norm_pd.
+         apply Rplus_le_compat_r. apply Rmult_le_compat_r.
+         apply Rplus_le_le_0_compat; try nra; try apply default_rel_ge_0.
+         by apply vec_norm_A1_rel.
     * apply Rmult_le_compat_l.
       ++ repeat apply Rmult_le_pos; try nra; try apply bpow_ge_0; try apply pos_INR.
          apply Rplus_le_le_0_compat; try nra; try apply g_pos.
