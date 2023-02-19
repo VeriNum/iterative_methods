b8116801c20787499d2710bd23e0f816156015ee
Mohit Tekriwal <tmohit@umich.edu>
2023-02-19 07:02:32 +0530

    updated

--- jacobi_iteration_bound.v
+++ jacobi_iteration_bound.v
@@ -1021,10 +1021,12 @@ apply Rinv_lt_contravar.
   admit.
 Admitted.
 
-(*
-Lemma 
-  (0 <= / ln (1 / rho_def A b))%Re
-*)
+
+Lemma ln_rho_inv_ge_0 {t: type} {n:nat}
+  (A: 'M[ftype t]_n.+1) (b: 'cV[ftype t]_n.+1):
+  (0 < rho_def_alt A b)%Re ->
+  (0 <= / ln (1 / rho_def A b))%Re.
+Proof.
 
 (** Refactoring definitions to make them readable and beautiful **)
 Lemma jacobi_precond_compute_implies_math {t: type} {n:nat}
