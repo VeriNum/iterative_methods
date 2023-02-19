e6112561e235bc479198ca12581fd8df5125da36
Mohit Tekriwal <tmohit@umich.edu>
2023-02-19 11:48:23 +0530

    started the proof of 0 < d_mag

--- jacobi_iteration_bound.v
+++ jacobi_iteration_bound.v
@@ -704,7 +704,17 @@ apply Rplus_lt_le_0_compat.
          (apply Rplus_le_le_0_compat; try nra; try by apply default_rel_ge_0).
          apply Rplus_le_le_0_compat; last by apply g_pos.
          apply Rplus_le_le_0_compat.
-         --
+         -- repeat apply Rmult_le_pos; try by apply g_pos.
+            apply Rplus_le_le_0_compat; try nra; try apply g_pos.
+            apply Rplus_le_le_0_compat; try nra; try apply default_rel_ge_0.
+         -- apply Rmult_le_pos; first by apply default_rel_ge_0.
+            apply Rplus_le_le_0_compat; try nra; try apply g_pos.
+      ++ apply Rmult_le_pos; last by (apply /RleP; apply matrix_norm_pd).
+         apply Rmult_le_pos.
+         -- apply Rplus_le_le_0_compat; last by apply default_abs_ge_0.
+            apply /RleP. apply vec_norm_pd.
+         -- apply Rlt_le, Rinv_0_lt_compat.
+            apply Rlt_Rminus. apply default_rel_ub_strict.
 
 
 
