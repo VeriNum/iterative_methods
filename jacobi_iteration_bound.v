ff182ef752d582f4afeba40a8ee1fc6fb0485405
Mohit Tekriwal <tmohit@umich.edu>
2023-02-21 18:07:09 +0530

    updated

--- jacobi_iteration_bound.v
+++ jacobi_iteration_bound.v
@@ -3913,6 +3913,35 @@ assert (is_finite (fprec t)
 repeat split; try apply H4; try (apply bmult_overflow_implies in H4; apply H4).
 Admitted.
 
+Lemma finite_implies_2 {t: type} :
+ forall (A: matrix t) (b: vector t),
+  let x0 := (repeat  (Zconst t 0) (length b)) in
+  let resid := jacobi_residual (diag_of_matrix A) (remove_diag A) b in
+  (0 < length A)%coq_nat ->
+  length A = length b ->
+  let n := (length A).-1 in
+  let A':= (@matrix_inj _ A n.+1 n.+1) in
+  let b' := (@vector_inj _ b n.+1) in
+  let x0' := (@vector_inj _ x0 n.+1) in
+  is_finite (fprec t) (femax t)
+  (norm2 (resid (jacobi_n A b x0 0))) = true ->
+  (forall xy : ftype t * ftype t,
+            In xy
+                (combine
+                    (vec_to_list_float n.+1
+                        (diag_vector_mult
+                            (A1_inv_J A')
+                                 (b' -f A2_J A' *f x0')))
+                                  (vec_to_list_float n.+1 x0')) ->
+             is_finite (fprec t) (femax t) xy.1 = true /\
+             is_finite (fprec t) (femax t) xy.2 = true /\
+             is_finite (fprec t) (femax t)
+                            (BPLUS t xy.1 (BOPP t xy.2)) = true).
+Proof.
+
+
+
+
 
 Lemma jacobi_iteration_bound_lowlevel {t: type} :
  forall (A: matrix t) (b: vector t) (acc: ftype t) (k: nat),
