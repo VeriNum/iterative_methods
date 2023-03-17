0dc96da893577e905ac337817d206ff3ec91d57b
Mohit Tekriwal <tmohit@umich.edu>
2023-03-17 11:59:45 -0400

    starting a new file to reorganize jacobi preconditions defintion

---
+++ jacobi_preconditions.v
@@ -0,0 +1,28 @@
+Require Import vcfloat.VCFloat vcfloat.FPLib.
+From mathcomp Require Import all_ssreflect ssralg  ssrnat all_algebra seq matrix .
+From mathcomp.analysis Require Import Rstruct.
+Require Import fma_is_finite dotprod_model.
+Require Import floatlib jacob_list_fun_model.
+Require Import fma_real_func_model fma_floating_point_model.
+Require Import inf_norm_properties common.
+Require Import norm_compat.
+
+Require Import Coq.Lists.List. Import ListNotations.
+Set Bullet Behavior "Strict Subproofs".
+
+Require Import lemmas.
+Require Import fma_dot_acc fma_matrix_vec_mult.
+From Flocq Require Import Binary.
+Require Import finite_lemmas_additional.
+Require Import fma_jacobi_forward_error.
+Require Import float_acc_lems.
+Require Import vec_sum_inf_norm_rel.
+Require Import fma_dot_mat_model.
+
+Section WITH_NANS.
+
+Context {NANS: Nans}.
+
+
+
+End WITH_NANS.
