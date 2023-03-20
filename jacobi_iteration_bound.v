0018771142eda3eca44b6dd27ab1003d072ed95e
Mohit Tekriwal <tmohit@umich.edu>
2023-03-20 18:12:00 -0400

    fixing the N = 0 case

--- jacobi_iteration_bound.v
+++ jacobi_iteration_bound.v
@@ -6000,6 +6000,29 @@ unfold is_finite in H.
 destruct x;unfold BMINUS, BINOP, Bminus; simpl in *; auto.
 Admitted.
 
+Lemma resid_sub_0_N_0 {t: type} :
+ forall (A: matrix t) (b: vector t),
+  let x0 := (repeat  (Zconst t 0) (length b)) in
+  let resid := jacobi_residual (diag_of_matrix A) (remove_diag A) b in
+  (0 < length A)%coq_nat ->
+  length A = length b ->
+  let n := (length A).-1 in
+  let A' := @matrix_inj _ A n.+1 n.+1 in
+  let b' := @vector_inj _ b n.+1 in
+  let x0' := @vector_inj _ x0 n.+1 in
+  @size_constraint t (length A).-1 ->
+  (forall i j, finite (A2_J A' i j)) ->
+  (forall i, finite (x0' i ord0)) ->
+  input_bound_at_N_0 A x0 b ->
+  (forall i, finite (A' i i)) ->
+  (forall i, finite (A1_inv_J A' i ord0)) ->
+  (forall i, finite (b' i ord0)) ->
+  matrix_inf_norm (FT2R_mat (A2_J A')) = 0%Re ->
+  FT2R_mat
+       (X_m_jacobi 2 x0' b' A' -f
+        X_m_jacobi 1 x0' b' A') = 0.
+Admitted.
+
 
 Lemma finite_residual_1 {t: type} :
  forall (A: matrix t) (b: vector t),
@@ -6120,6 +6143,9 @@ admit. } rewrite H8. split.
 
 
 Admitted.
+
+
+
 
 Lemma jacobi_iteration_bound_lowlevel {t: type} :
  forall (A: matrix t) (b: vector t) (acc: ftype t) (k: nat),
