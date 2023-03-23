1cc948acf50176a019a417aaf2cf0345eccf812f
Mohit Tekriwal <tmohit@umich.edu>
2023-03-22 20:02:31 -0400

    refactoring the proof

--- jacobi_iteration_bound.v
+++ jacobi_iteration_bound.v
@@ -3664,6 +3664,76 @@ apply H6.
 + by apply bound_each_elem_A2_x0 .
 Qed.
 
+
+Lemma dotprod_r_eq_0_r {t} (v : list (ftype t * ftype t)):
+  (forall i, (i < length v)%coq_nat ->
+             nth ((length v).-1 - i)%coq_nat (snd (List.split v)) (Zconst t 0) = Zconst t 0 \/
+             nth ((length v).-1 - i)%coq_nat (snd (List.split v)) (Zconst t 0) = neg_zero) ->
+  (forall i, (i < length v)%coq_nat ->
+             finite (nth ((length v).-1 - i)%coq_nat  (fst (List.split v)) (Zconst t 0))) ->
+  dotprod_r (fst (List.split v)) (snd (List.split v)) = Zconst t 0 .
+Proof.
+intros.
+induction v.
++ simpl. unfold dotprod_r. by unfold combine;simpl.
++ unfold dotprod_r. rewrite list_split_l list_split_r.
+  simpl.
+  assert (forall i : nat, (i < length v)%coq_nat ->
+             nth ((length v).-1 - i)%coq_nat (split v).2 (Zconst t 0) = Zconst t 0 \/
+             nth ((length v).-1 - i)%coq_nat (split v).2 (Zconst t 0) = neg_zero).
+  { intros. specialize (H i).
+    assert ((i < length (a :: v))%coq_nat).
+    { simpl. lia. } specialize (H H2).
+    assert (((length (a :: v)).-1 - i)%coq_nat = 
+            (((length v).-1 - i).+1)%coq_nat) .
+    { simpl. rewrite -subSn /=.
+      destruct (length v). contradict H1. lia.
+      simpl. destruct i. by rewrite subn0. rewrite subSS. 
+      by []. rewrite -ltnS.
+      destruct (length v). contradict H1. lia. 
+      simpl. by apply /ssrnat.ltP.
+    } rewrite H3 in H. 
+    by rewrite ?list_split_l ?list_split_r /= in H.
+  } specialize (IHv H1).
+  unfold dotprod_r in IHv. rewrite IHv.
+  specialize (H (length (a :: v)).-1).
+  assert (((length (a :: v)).-1 < length (a :: v))%coq_nat).
+  { apply /ssrnat.ltP. apply ltnSn. } specialize (H H2).
+  assert (((length (a :: v)).-1 - (length (a :: v)).-1)%coq_nat = 0%nat).
+  { lia. } rewrite H3 in H. 
+  rewrite ?list_split_l ?list_split_r /= in H.
+  specialize (H0 (length (a :: v)).-1 H2 ).
+  rewrite H3 in H0.
+  rewrite ?list_split_l ?list_split_r /= in H0.
+  destruct H.
+  - rewrite H. auto.
+    destruct a.1; try contradiction; simpl;auto.
+    destruct s; auto.
+    unfold BFMA, Bfma, BSN2B,BinarySingleNaN.Bfma.
+    simpl. unfold BinarySingleNaN.Bfma_szero.
+    simpl. destruct s; simpl; auto.
+  - rewrite H. auto.
+    destruct a.1; try contradiction; simpl;auto.
+    destruct s; auto. 
+    unfold BFMA, Bfma, BSN2B,BinarySingleNaN.Bfma.
+    simpl. unfold BinarySingleNaN.Bfma_szero.
+    simpl. destruct s; simpl; auto.
+  - intros.
+    specialize (H0 i).
+    assert ((i < length (a :: v))%coq_nat).
+    { simpl. lia. } specialize (H0 H3).
+    assert (((length (a :: v)).-1 - i)%coq_nat = 
+            (((length v).-1 - i).+1)%coq_nat) .
+    { simpl. rewrite -subSn /=.
+      destruct (length v). contradict H1. lia.
+      simpl. destruct i. by rewrite subn0. rewrite subSS. 
+      by []. rewrite -ltnS.
+      destruct (length v). contradict H1. lia. 
+      simpl. by apply /ssrnat.ltP.
+    } rewrite H4 in H0. 
+    by rewrite ?list_split_l ?list_split_r /= in H0.
+Qed.
+
 Lemma A2_mult_x0_eq_0 {t: type} :
   forall (A: matrix t) (b: vector t),
   let x0 := (repeat  (Zconst t 0) (length b)) in
