6e91e281fbb8f6333c51e82ded05fff1c2bfd003
Mohit Tekriwal <tmohit@umich.edu>
2023-04-01 16:35:07 -0400

    figured out how to use mathcomp limits

--- bound_on_x.v
+++ bound_on_x.v
@@ -1,6 +1,5 @@
-Require Import vcfloat.VCFloat vcfloat.FPLib.
 From mathcomp Require Import all_ssreflect ssralg  ssrnat all_algebra seq matrix .
-From mathcomp.analysis Require Import Rstruct sequences.
+From mathcomp.analysis Require Import Rstruct sequences normedtype landau topology.
 Require Import fma_is_finite dotprod_model.
 Require Import floatlib jacob_list_fun_model.
 Require Import fma_real_func_model fma_floating_point_model.
@@ -19,6 +18,18 @@ Require Import float_acc_lems.
 Require Import vec_sum_inf_norm_rel.
 Require Import fma_dot_mat_model.
 Require Import jacobi_preconditions.
+
+From mathcomp.classical Require Import boolp classical_sets functions.
+
+Import Order.TTheory GRing.Theory Num.Def Num.Theory.
+Import numFieldNormedType.Exports.
+
+Open Scope classical_set_scope.
+
+Open Scope ring_scope.
+
+Delimit Scope ring_scope with Ri.
+Delimit Scope R_scope with Re.
 
 Section WITH_NANS.
 
@@ -59,8 +70,9 @@ Lemma x_bound_exists {t} {n:nat}
 Proof.
 intros.
 remember (\col_(j < n.+1) 0%Re) as x0.
+Print topology.lim.
 apply Rle_trans with
-  (topology.lim (series (fun k : nat => vec_inf_norm (x_k k x0 b_real A_real)))).
+  (lim (fun j: nat => series (fun k : nat => vec_inf_norm (x_k k x0 b_real A_real)) j )).
 
 
 
