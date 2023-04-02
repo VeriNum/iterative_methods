816bff0caea0fe3ac1aa9d521da2aefc8e6b9f6e
Mohit Tekriwal <tmohit@umich.edu>
2023-04-02 08:52:38 -0400

    updated

--- bound_on_x.v
+++ bound_on_x.v
@@ -1,5 +1,6 @@
+Require Import vcfloat.VCFloat vcfloat.FPLib.
 From mathcomp Require Import all_ssreflect ssralg  ssrnat all_algebra seq matrix .
-From mathcomp.analysis Require Import Rstruct sequences normedtype landau topology.
+From mathcomp.analysis Require Import Rstruct.
 Require Import fma_is_finite dotprod_model.
 Require Import floatlib jacob_list_fun_model.
 Require Import fma_real_func_model fma_floating_point_model.
@@ -20,13 +21,8 @@ Require Import fma_dot_mat_model.
 Require Import jacobi_preconditions.
 From Coquelicot Require Import Coquelicot.
 
-
-From mathcomp.classical Require Import boolp classical_sets functions.
-
 Import Order.TTheory GRing.Theory Num.Def Num.Theory.
-Import numFieldNormedType.Exports.
 
-Open Scope classical_set_scope.
 
 Open Scope ring_scope.
 
@@ -36,8 +32,6 @@ Delimit Scope R_scope with Re.
 Section WITH_NANS.
 
 Context {NANS: Nans}.
-
-Print series.
 
 
 (** Check (series (fun n: nat => (vec_inf_norm x1))). **)
@@ -60,6 +54,128 @@ intros.
 by unfold Rbar_le in H.
 Qed.
 
+Lemma matrix_norm_A2_rel {t: type} {n:nat}
+  (A: 'M[ftype t]_n.+1):
+  (matrix_inf_norm
+   (A2_J_real (FT2R_mat A)) <=
+ matrix_inf_norm (FT2R_mat (A2_J A)))%Re.
+Proof.
+assert (A2_J_real (FT2R_mat A) = 
+        FT2R_mat (A2_J A)).
+{ apply /matrixP. unfold eqrel. intros. rewrite !mxE.
+  case: (x == y :> nat); simpl; nra.
+} rewrite H; nra.
+Qed.
+
+
+Lemma bpow_fprec_lb_strict t : 
+(2 < bpow Zaux.radix2 (fprec t))%Re.
+Proof. 
+pose proof fprec_gt_one t.
+eapply Rle_lt_trans with (bpow Zaux.radix2 1).
+unfold bpow; simpl; nra.
+apply bpow_lt; lia.
+Qed.
+
+Local Open Scope Z_scope.
+Lemma default_abs_ub_strict t :
+(default_abs t < 1)%Re.
+Proof.
+unfold default_abs.
+pose proof bpow_gt_0 Zaux.radix2 (femax t).
+pose proof bpow_gt_0 Zaux.radix2 (fprec t).
+replace (3 - femax t - fprec t)%Z with (3 +- femax t +- fprec t)%Z by lia.
+rewrite !bpow_plus.
+rewrite <- !Rmult_assoc.
+replace (/ 2 * bpow Zaux.radix2 3)%Re with 4%Re; [|simpl;nra].
+rewrite !bpow_opp !Rcomplements.Rlt_div_r. 
+field_simplify; try nra.
+assert ( (2 * 2 < (/ / bpow Zaux.radix2 (fprec t)) *(/
+                / bpow Zaux.radix2 (femax t)))%Re ->
+        (2 <
+           1 / / bpow Zaux.radix2 (fprec t) /
+           / bpow Zaux.radix2 (femax t) / 2)%Re).
+{ intros. nra. } apply H1. repeat (rewrite Rinv_involutive; try nra).
+apply Rmult_lt_compat; try nra.
+apply bpow_fprec_lb_strict.
+apply Rlt_le_trans with 4%Re.
+nra. apply bpow_femax_lb.
+nra. 
+apply Rlt_gt. apply Rinv_0_lt_compat. apply bpow_gt_0.
+apply Rlt_gt. apply Rinv_0_lt_compat. apply bpow_gt_0.
+Qed.
+
+Lemma default_rel_ub_strict t :
+(default_rel t < 1)%Re.
+Proof.
+unfold default_rel.
+pose proof bpow_gt_0 Zaux.radix2 (fprec t).
+rewrite !bpow_plus.
+rewrite <- !Rmult_assoc.
+rewrite Rmult_comm.
+rewrite <- !Rmult_assoc.
+replace (bpow Zaux.radix2 1 * / 2)%Re with 1%Re; [|simpl;nra].
+rewrite !bpow_opp !Rcomplements.Rlt_div_r. 
+field_simplify; try nra.
+replace 1%Re with  (bpow Zaux.radix2 0).
+apply bpow_lt. apply fprec_gt_0.
+simpl; auto.
+apply Rlt_gt;
+replace (/ bpow Zaux.radix2 (fprec t))%Re with (1 / bpow Zaux.radix2 (fprec t))%Re by nra;
+apply Rdiv_lt_0_compat; try nra.
+Qed.
+
+Close Scope Z_scope.
+
+Lemma vec_norm_A1_rel {t: type} {n:nat}
+  (A: 'M[ftype t]_n.+1)
+(Hinv: forall i, finite (BDIV (Zconst t 1) (A i i)))
+(Ha : forall i j, finite (A i j)):
+(vec_inf_norm (A1_diag (FT2R_mat A))  <=
+ (vec_inf_norm (FT2R_mat (A1_inv_J A)) + default_abs t) / (1 - default_rel t) )%Re.
+Proof.
+unfold vec_inf_norm.
+apply bigmax_le.
++ by rewrite size_map size_enum_ord.
++ intros.
+  rewrite seq_equiv. rewrite nth_mkseq;
+  last by rewrite size_map size_enum_ord in H.
+  rewrite mxE.
+  apply Rcomplements.Rle_div_r. apply Rlt_Rminus.
+  apply default_rel_ub_strict.
+  apply Rcomplements.Rle_minus_l.
+  apply Rle_trans with
+  [seq Rabs
+          (FT2R_mat (A1_inv_J A) i0 0)
+      | i0 <- enum 'I_n.+1]`_i.
+  - rewrite seq_equiv. rewrite nth_mkseq;
+    last by rewrite size_map size_enum_ord in H.
+    rewrite !mxE.
+    pose proof (@Binv_accurate _ t (A (inord i) (inord i)) (Hinv (@inord n i)) (Ha (@inord n i) (@inord n i)) ).
+    destruct H0 as [d [e [Hde [Hd [He H0]]]]].
+    rewrite H0.
+    assert (e = (- - e)%Re). 
+    { symmetry. apply Ropp_involutive. } rewrite H1.
+    eapply Rle_trans; last by apply Rabs_triang_inv.
+    rewrite Rabs_Ropp.
+    apply Rplus_le_compat.
+    rewrite Rabs_mult. rewrite real_const_1.
+    apply Rmult_le_compat_l. apply Rabs_pos.
+    assert (d = (- - d)%Re). 
+    { symmetry. apply Ropp_involutive. } rewrite H2.
+    eapply Rle_trans; try apply Rabs_triang_inv.
+    rewrite Rabs_Ropp. rewrite Rabs_R1.
+    apply Rplus_le_compat_l. 
+    apply Ropp_le_contravar. apply Hd.
+    apply Ropp_le_contravar. apply He.
+  - apply /RleP.
+    apply (@bigmaxr_ler _ 0%Re [seq Rabs
+                                   (FT2R_mat (A1_inv_J A) i0 0)
+                               | i0 <- enum 'I_n.+1] i).
+    rewrite size_map size_enum_ord.
+    by rewrite size_map size_enum_ord in H.
+Qed.
+
 
 Lemma R_def_lt_1_implies {t} {n:nat}
   (A : 'M[ftype t]_n.+1) :
@@ -71,6 +187,16 @@ Lemma R_def_lt_1_implies {t} {n:nat}
   (vec_inf_norm (A1_diag A_real) *
     matrix_inf_norm (A2_J_real A_real) <1)%Re.
 Proof.
+intros.
+apply Rle_lt_trans with R_def; last by apply H.
+unfold R_def. 
+assert (A2_J_real A_real = A2_real).
+{ apply matrixP. unfold eqrel. intros. rewrite !mxE /=.
+  case: (x == y :> nat); simpl; auto.
+} rewrite H0. apply Rmult_le_compat_r.
+apply /RleP. apply matrix_norm_pd.
+
+
 
 
 Lemma x_bound_exists {t} {n:nat}
