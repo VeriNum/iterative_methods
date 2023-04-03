22ba0f846afb5181ee33c86a27a83da82d2c3c04
Mohit Tekriwal <tmohit@umich.edu>
2023-04-03 16:38:39 -0400

    Added a file for the proof of diagonal dominance ==> invertibility

---
+++ diag_invertibility.v
@@ -0,0 +1,89 @@
+Require Import vcfloat.VCFloat vcfloat.FPLib.
+From mathcomp Require Import all_ssreflect ssralg  ssrnat all_algebra seq matrix .
+From mathcomp.analysis Require Import Rstruct.
+Require Import fma_is_finite dotprod_model.
+Require Import floatlib jacob_list_fun_model.
+Require Import fma_real_func_model fma_floating_point_model.
+Require Import inf_norm_properties common.
+Require Import norm_compat.
+
+Require Import Coq.Lists.List. Import ListNotations.
+Set Bullet Behavior "Strict Subproofs".
+
+Require Import lemmas.
+Require Import fma_dot_acc fma_matrix_vec_mult.
+From Flocq Require Import Binary.
+Require Import finite_lemmas_additional.
+Require Import fma_jacobi_forward_error.
+Require Import float_acc_lems.
+Require Import vec_sum_inf_norm_rel.
+Require Import fma_dot_mat_model.
+Require Import jacobi_preconditions.
+Require Import bound_on_x.
+
+Section WITH_NANS.
+
+Context {NANS: Nans}.
+
+Lemma transpose_implies {n}:
+  forall v1 v2: 'rV[R]_n,
+  v1^T = v2^T ->
+  v1 = v2.
+Proof.
+intros.
+apply matrixP. unfold eqrel. intros.
+apply matrixP in H. unfold eqrel in H.
+specialize (H y x).
+by rewrite !mxE in H.
+Qed.
+
+Lemma vec_is_zero_or_not: 
+  forall (n:nat) (x : 'cV[R]_n.+1),
+   x = 0 \/ x != 0.
+Admitted.
+
+Lemma transpose_idempotent {m n:nat} (A: 'M[R]_(m,n)):
+  A = (A^T)^T.
+Proof.
+apply matrixP. unfold eqrel. intros.
+by rewrite !mxE. 
+Qed.
+
+
+Lemma diagonal_dominance_implies_invertibility {t} {n:nat} 
+  (A: 'M[ftype t]_n.+1):
+  strict_diagonal_dominance A ->
+  (FT2R_mat A) \in unitmx.
+Proof.
+intros.
+rewrite -unitmx_tr.
+unfold strict_diagonal_dominance in H.
+rewrite -row_free_unit.
+apply inj_row_free. intros.
+rewrite -H0. apply transpose_implies.
+rewrite trmx_mul. symmetry.
+rewrite -transpose_idempotent.
+remember (v^T) as v_c.
+rewrite -Heqv_c.
+remember (FT2R_mat A) as A_real.
+assert (v_c = 0 \/ v_c != 0).
+{ by apply vec_is_zero_or_not. }
+destruct H1.
++ rewrite H1. by rewrite mulmx0.
++ apply matrixP. unfold eqrel. intros.
+  rewrite !mxE.
+  rewrite (bigD1 x) /=.
+  apply Rle_antisym.
+  
+
+
+
+
+
+Admitted.
+
+
+
+
+
+End WITH_NANS.
