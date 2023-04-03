ef4f49ea48e1bc9bcc38a48e6c03eb8b22d7c627
Mohit Tekriwal <tmohit@umich.edu>
2023-04-02 21:49:45 -0400

    some reorganziations

--- fma_jacobi_forward_error.v
+++ fma_jacobi_forward_error.v
@@ -38,59 +38,6 @@ Notation "-f A" := (opp_mat A) (at level 50).
 Notation "A *f B" := (mulmx_float A B) (at level 70).
 Notation "A -f B" := (sub_mat A B) (at level 80).
 
-Lemma vec_inf_norm_diag_matrix_vec_mult_R {n:nat} (v1 v2 : 'cV[R]_n.+1):
-  vec_inf_norm (diag_matrix_vec_mult_R v1 v2) <= 
-  vec_inf_norm v1 * vec_inf_norm v2.
-Proof.
-unfold vec_inf_norm, diag_matrix_vec_mult_R.
-rewrite -bigmaxr_mulr.
-+ apply /RleP. apply bigmax_le.
-  - by rewrite size_map size_enum_ord.
-  - intros. rewrite seq_equiv. rewrite nth_mkseq; 
-    last by rewrite size_map size_enum_ord in H.
-    apply Rle_trans with 
-    [seq (bigmaxr 0%Re
-           [seq Rabs (v1 i1 0) | i1 <- enum 'I_n.+1] *
-         Rabs (v2 i0 0))%Ri
-      | i0 <- enum 'I_n.+1]`_i.
-    * assert ([seq bigmaxr 0%Re
-                    [seq Rabs (v1 i1 0) | i1 <- enum 'I_n.+1] *
-                  Rabs (v2 i0 0)
-                | i0 <- enum 'I_n.+1] = 
-               mkseq (fun i: nat => bigmaxr 0%Re
-                            [seq Rabs (v1 i1 0) | i1 <- enum 'I_n.+1] *
-                            Rabs (v2 (@inord n i) 0))
-                             n.+1).
-      { by rewrite !seq_equiv. } rewrite H0.
-      rewrite nth_mkseq; 
-      last by rewrite size_map size_enum_ord in H.
-      rewrite !mxE. rewrite -!RmultE. rewrite Rabs_mult.
-      rewrite !nth_vec_to_list_real; try rewrite inord_val.
-      ++ apply Rmult_le_compat_r; try apply Rabs_pos.
-         apply Rle_trans with 
-         [seq Rabs (v1 i1 0) | i1 <- enum 'I_n.+1]`_i.
-         -- rewrite seq_equiv. rewrite nth_mkseq; 
-            last by rewrite size_map size_enum_ord in H.
-            apply Rle_refl.
-         -- apply /RleP.
-            apply (@bigmaxr_ler _ 0%Re [seq Rabs (v1 i1 0) | i1 <- enum 'I_n.+1] i).
-            rewrite size_map size_enum_ord.
-            by rewrite size_map size_enum_ord in H.
-      ++ by rewrite size_map size_enum_ord in H.
-      ++ by rewrite size_map size_enum_ord in H.
-    * apply /RleP.
-      apply (@bigmaxr_ler _ 0%Re [seq bigmaxr 0%Re
-                     [seq Rabs (v1 i1 0) | i1 <- enum 'I_n.+1] *
-                   Rabs (v2 i0 0)
-                 | i0 <- enum 'I_n.+1] i).
-       rewrite size_map size_enum_ord.
-       by rewrite size_map size_enum_ord in H.
-+ apply bigmax_le_0.
-  - apply /RleP. apply Rle_refl.
-  - intros. rewrite seq_equiv. rewrite nth_mkseq;
-    last by rewrite size_map size_enum_ord in H.
-    apply /RleP. apply Rabs_pos.
-Qed.
 
 
 Definition f_error {ty} {n:nat} m b x0 x (A: 'M[ftype ty]_n.+1):=
@@ -230,60 +177,6 @@ Lemma sub_vec_3 {n:nat}:
 Proof.
 intros. apply matrixP. unfold eqrel.
 intros. rewrite !mxE. rewrite -!RplusE -!RoppE. nra.
-Qed.
-
-
-
-Lemma x_fixpoint {n:nat} x b (A: 'M[R]_n.+1):
-  A *m x = b ->
-  (forall i, A i i <> 0%Re) ->
-  x = x_fix x b A.
-Proof.
-intros.
-unfold x_fix. unfold diag_matrix_vec_mult_R.
-apply /matrixP. unfold eqrel. intros.
-rewrite !mxE. rewrite !nth_vec_to_list_real.
-+ rewrite !mxE. 
-  assert (x x0 y = ((1 / A (inord x0) (inord x0)) *
-                    (A (inord x0) (inord x0) * x x0 y))%Re).
-  { assert (((1 / A (inord x0) (inord x0)) *
-                    (A (inord x0) (inord x0) * x x0 y))%Re = 
-             ((A (inord x0) (inord x0) * / A (inord x0) (inord x0))*
-              x x0 y)%Re).
-    { nra. } rewrite H1. rewrite Rinv_r.
-    nra.  apply H0.
-  } rewrite H1.
-  assert ((((A (inord x0) (inord x0)) * x x0 y))%Re  = 
-           (b (inord x0) ord0 -
-              \sum_j A2_J_real (A) (inord x0) j * x j ord0)%Re).   
-  { assert (forall x y z:R, (x + y = z)%Re -> (x = z - y)%Re).
-    { intros. nra. } apply H2.
-    assert (( (A (inord x0) (inord x0)) * x x0 y +
-              \sum_j A2_J_real A (inord x0) j * x j ord0)%Re = 
-              \sum_j (A x0 j * x j ord0)%Re).
-    { unfold A2_J_real. rewrite [in RHS](bigD1 x0) /=.
-      rewrite inord_val. 
-      assert (y = ord0). { by apply ord1. } rewrite H3. 
-      apply Rplus_eq_compat_l. 
-      assert (\sum_(i < n.+1 | i != x0)
-                    (A x0 i * x i ord0)%Re = 
-               \sum_(i < n.+1)
-                   (if (~~ (i == x0 :> nat)) then 
-                      (A x0 i * x i ord0)%Re else 0%Re)).
-      { by rewrite big_mkcond /=. } rewrite H4.
-      apply eq_big.
-      by []. intros. rewrite !mxE. rewrite eq_sym.
-      destruct (i == x0 :>nat).
-      + simpl. by rewrite mul0r.
-      + simpl. by rewrite -RmultE.
-      + by [].
-    } rewrite H3. apply matrixP in H. unfold eqrel in H.
-    specialize (H x0 y). rewrite !mxE in H.
-    assert (y = ord0). { by apply ord1. } rewrite H4 in H.
-    rewrite inord_val. by apply H.
-  } rewrite H2. by [].
-+ apply ltn_ord.
-+ apply ltn_ord.
 Qed.
 
 
