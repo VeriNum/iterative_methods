dcf542b6e77cda1a765083aed1b7f849fafc6bb6
Mohit Tekriwal <tmohit@umich.edu>
2023-04-02 22:00:00 -0400

    minor cleanups

--- bound_on_x.v
+++ bound_on_x.v
@@ -1,13 +1,11 @@
 Require Import vcfloat.VCFloat vcfloat.FPLib.
 From mathcomp Require Import all_ssreflect ssralg  ssrnat all_algebra seq matrix .
 From mathcomp.analysis Require Import Rstruct.
-Require Import fma_is_finite dotprod_model.
-Require Import floatlib jacob_list_fun_model.
-Require Import fma_real_func_model fma_floating_point_model.
+Require Import fma_real_func_model fma_floating_point_model .
 Require Import inf_norm_properties common.
-Require Import norm_compat.
 
-Require Import Coq.Lists.List. Import ListNotations.
+Require Import Coq.Lists.List. 
+Import ListNotations.
 Set Bullet Behavior "Strict Subproofs".
 
 Require Import lemmas.
@@ -15,9 +13,6 @@ Require Import finite_lemmas_additional.
 Require Import fma_jacobi_forward_error. 
 Require Import float_acc_lems.
 From Coquelicot Require Import Coquelicot.
-
-Import Order.TTheory GRing.Theory Num.Def Num.Theory.
-
 
 Open Scope ring_scope.
 
@@ -27,10 +22,6 @@ Delimit Scope R_scope with Re.
 Section WITH_NANS.
 
 Context {NANS: Nans}.
-
-
-(** Check (series (fun n: nat => (vec_inf_norm x1))). **)
-
 
 (** Define the solution vector at k-th iteration **)
 Fixpoint x_k {n:nat} (k: nat) 
