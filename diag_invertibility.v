6873a4c0654cd8c34e037657f7bea61e80237b65
Mohit Tekriwal <tmohit@umich.edu>
2023-04-03 21:35:57 -0400

    minor cleanups

--- diag_invertibility.v
+++ diag_invertibility.v
@@ -4,15 +4,12 @@ From mathcomp.analysis Require Import Rstruct.
 Require Import fma_is_finite dotprod_model.
 Require Import floatlib jacob_list_fun_model.
 Require Import fma_real_func_model fma_floating_point_model.
-Require Import inf_norm_properties common.
-Require Import norm_compat.
+Require Import inf_norm_properties. 
 
 Require Import Coq.Lists.List. Import ListNotations.
 Set Bullet Behavior "Strict Subproofs".
 
 Require Import lemmas.
-Require Import fma_dot_acc fma_matrix_vec_mult.
-From Flocq Require Import Binary.
 Require Import jacobi_preconditions.
 
 Section WITH_NANS.
