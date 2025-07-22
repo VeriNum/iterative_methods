5e2bf45cf6c2955f58dbf9c7a8bd3fda00ba0e26
Yichen Tao <taoyc0904@sjtu.edu.cn>
2025-07-22 12:54:13 -0400

    defined coog (coordinate form graph/shell) and related orders:

---
+++ sparse/partial_csrg.v
@@ -0,0 +1,10 @@
+Require Import VST.floyd.proofauto.
+Require Import Iterative.floatlib.
+From Iterative.sparse Require Import sparse_model distinct partial_csr.
+Require Import vcfloat.FPStdCompCert.
+Require Import vcfloat.FPStdLib.
+Require Import Coq.Classes.RelationClasses.
+
+Set Bullet Behavior "Strict Subproofs".
+
+
